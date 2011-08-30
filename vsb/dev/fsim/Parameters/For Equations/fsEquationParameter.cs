1e8e622f0111c5621251b42071920d07d49ecc20
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2011-08-30 16:42:46 +0000

    starting refactoring for equation parameters

---
+++ vsb/dev/fsim/Parameters/For Equations/fsEquationParameter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParametersIdentifiers.For_Equations
+{
+    public class fsEquationParameter : fsJustValueParameter
+    {
+    }
+}
