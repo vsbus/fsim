e1084775f5dfaf738cd5ad4d889f8815a5eb3d24
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2011-09-11 14:35:15 +0000

    resharped Units

--- vsb/dev/fsim/Units/fsUnits.cs
+++ vsb/dev/fsim/Units/fsUnits.cs
@@ -1,43 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Units
+﻿namespace Units
 {
     public class fsUnits
     {
         #region fsUnits
 
-        struct UnitRecord
+        struct fsUnitRecord
         {
-            string m_name;
-            public string Name
-            {
-                get { return m_name; }
-                set { m_name = value; }
-            }
+            public string Name { get; private set; }
 
-            double m_coefficient;
-            public double Coefficient
-            {
-                get { return m_coefficient; }
-                set { m_coefficient = value; }
-            }
+            public double Coefficient { get; private set; }
 
-            public UnitRecord(string name, double coefficient)
+            public fsUnitRecord(string name, double coefficient) : this()
             {
-                m_name = name;
-                m_coefficient = coefficient;
+                Name = name;
+                Coefficient = coefficient;
             }
         }
 
-        private UnitRecord [] m_units;
-        private UnitRecord m_currentUnit;
+        private readonly fsUnitRecord [] m_units;
+        private fsUnitRecord m_currentUnit;
 
-        private fsUnits(params UnitRecord[] units)
+        private fsUnits(params fsUnitRecord[] units)
         {
-            m_units = new UnitRecord[units.Length];
+            m_units = new fsUnitRecord[units.Length];
             for (int i = 0; i < units.Length; ++i)
             {
                 m_units[i] = units[i];
@@ -65,73 +50,73 @@ namespace Units
 
         #region Units Collection
 
-        public static fsUnits NoUnits = new fsUnits(new UnitRecord[] {
-            new UnitRecord("-", 1),
+        public static fsUnits NoUnits = new fsUnits(new[] {
+            new fsUnitRecord("-", 1),
         });
 
-        public static fsUnits Length = new fsUnits(new UnitRecord[] {
-            new UnitRecord("mm", 1e-3),
-            new UnitRecord("m", 1)
+        public static fsUnits Length = new fsUnits(new[] {
+            new fsUnitRecord("mm", 1e-3),
+            new fsUnitRecord("m", 1)
         });
 
-        public static fsUnits Area = new fsUnits(new UnitRecord[] {
-            new UnitRecord("m2", 1),
-            new UnitRecord("cm2", 1e-4),
-            new UnitRecord("mm2", 1e-6)
+        public static fsUnits Area = new fsUnits(new[] {
+            new fsUnitRecord("m2", 1),
+            new fsUnitRecord("cm2", 1e-4),
+            new fsUnitRecord("mm2", 1e-6)
         });
 
-        public static fsUnits Pressure = new fsUnits(new UnitRecord[] {
-            new UnitRecord("bar", 1e5),
-            new UnitRecord("Pa", 1)
+        public static fsUnits Pressure = new fsUnits(new[] {
+            new fsUnitRecord("bar", 1e5),
+            new fsUnitRecord("Pa", 1)
         });
 
-        public static fsUnits Time = new fsUnits(new UnitRecord[] {
-            new UnitRecord("s", 1),
-            new UnitRecord("m", 60)
+        public static fsUnits Time = new fsUnits(new[] {
+            new fsUnitRecord("s", 1),
+            new fsUnitRecord("m", 60)
         });
 
-        public static fsUnits Frequency = new fsUnits(new UnitRecord[] {
-            new UnitRecord("1/s", 1),
-            new UnitRecord("1/m", 1.0 / 60)
+        public static fsUnits Frequency = new fsUnits(new[] {
+            new fsUnitRecord("1/s", 1),
+            new fsUnitRecord("1/m", 1.0 / 60)
         });
 
-        public static fsUnits Concentration = new fsUnits(new UnitRecord[] {
-            new UnitRecord("%", 1e-2),
-            new UnitRecord("-", 1)
+        public static fsUnits Concentration = new fsUnits(new[] {
+            new fsUnitRecord("%", 1e-2),
+            new fsUnitRecord("-", 1)
         });
 
-        public static fsUnits Density = new fsUnits(new UnitRecord[] {
-            new UnitRecord("kg/m3", 1),
+        public static fsUnits Density = new fsUnits(new[] {
+            new fsUnitRecord("kg/m3", 1),
         });
 
-        public static fsUnits Viscosity = new fsUnits(new UnitRecord[] {
-            new UnitRecord("mPa s", 1e-3),
+        public static fsUnits Viscosity = new fsUnits(new[] {
+            new fsUnitRecord("mPa s", 1e-3),
         });
 
-        public static fsUnits Mass = new fsUnits(new UnitRecord[] {
-            new UnitRecord("kg", 1),
-            new UnitRecord("g", 1e-3)
+        public static fsUnits Mass = new fsUnits(new[] {
+            new fsUnitRecord("kg", 1),
+            new fsUnitRecord("g", 1e-3)
         });
 
-        public static fsUnits Volume = new fsUnits(new UnitRecord[] {
-            new UnitRecord("l", 1e-3),
-            new UnitRecord("m3", 1)
+        public static fsUnits Volume = new fsUnits(new[] {
+            new fsUnitRecord("l", 1e-3),
+            new fsUnitRecord("m3", 1)
         });
 
-        public static fsUnits PermeabilityPc = new fsUnits(new UnitRecord[] {
-            new UnitRecord("10-13m2", 1e-13)
+        public static fsUnits PermeabilityPc = new fsUnits(new[] {
+            new fsUnitRecord("10-13m2", 1e-13)
         });
 
-        public static fsUnits CakeResistanceRc = new fsUnits(new UnitRecord[] {
-            new UnitRecord("10+13m-2", 1e13)
+        public static fsUnits CakeResistanceRc = new fsUnits(new[] {
+            new fsUnitRecord("10+13m-2", 1e13)
         });
 
-        public static fsUnits CakeResistanceAlpha = new fsUnits(new UnitRecord[] {
-            new UnitRecord("10+10m/kg", 1e10)
+        public static fsUnits CakeResistanceAlpha = new fsUnits(new[] {
+            new fsUnitRecord("10+10m/kg", 1e10)
         });
 
-        public static fsUnits FilterMediumResistance = new fsUnits(new UnitRecord[] {
-            new UnitRecord("10+10m/kg", 1e10)
+        public static fsUnits FilterMediumResistance = new fsUnits(new[] {
+            new fsUnitRecord("10+10m/kg", 1e10)
         });
 
         #endregion
