cf5f34f2b8f1a1838aac852258fcc949de0f43d9
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2011-10-11 10:32:16 +0000

    cake porosity calc fixed for not plain area

---
+++ vsb/dev/fsim/Equations/Math/fsAssignEquation.cs
@@ -0,0 +1,45 @@
+﻿using Parameters;
+
+namespace Equations
+{
+    public class fsAssignEquation : fsCalculatorEquation
+    {
+        // x = y
+
+        #region Parameters
+        
+        readonly IEquationParameter m_left;
+        readonly IEquationParameter m_right;
+        
+        #endregion
+
+        public fsAssignEquation(
+            IEquationParameter left,
+            IEquationParameter right)
+            : base (left, right)
+        {
+            m_left = left;
+            m_right = right;
+        }
+
+        protected override void InitFormulas()
+        {
+            AddFormula(m_left, LeftFormula);
+            AddFormula(m_right, RightFormula);
+        }
+
+        #region Formulas
+
+        private void LeftFormula()
+        {
+            m_left.Value = m_right.Value;
+        }
+
+        private void RightFormula()
+        {
+            m_right.Value = m_left.Value;
+        }
+
+        #endregion
+    }
+}
