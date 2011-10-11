a417d6d4e1bad1483efaa8e5db91e12714a428f8
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2011-10-11 10:29:13 +0000

    cake porosity calc fixed for not plain area

--- branches/developers/vsb/dev/fsim/StepCalculators/Small Calculators/fsCakePorosityCalculator.cs
+++ branches/developers/vsb/dev/fsim/StepCalculators/Small Calculators/fsCakePorosityCalculator.cs
@@ -9,32 +9,36 @@ namespace StepCalculators
 {
     public class fsCakePorosityCalculator : fsCalculator
     {
-        readonly fsCalculatorVariable m_area;
-        readonly fsCalculatorVariable m_diameter;
+        readonly fsCalculatorVariable m_machineArea;
+        readonly fsCalculatorVariable m_cakeArea;
+        readonly fsCalculatorVariable m_machineDiameter;
+        readonly fsCalculatorVariable m_filterElementDiameter;
         readonly fsCalculatorVariable m_b;
         readonly fsCalculatorVariable m_bOverD;
         readonly fsCalculatorConstant m_rhoL;
         readonly fsCalculatorConstant m_rhoS;
-        readonly fsCalculatorConstant m_hc;
-        readonly fsCalculatorConstant m_Mcw;
-        readonly fsCalculatorConstant m_Mcd;
-        readonly fsCalculatorConstant m_C;
+        readonly fsCalculatorConstant m_cakeHeight;
+        readonly fsCalculatorConstant m_wetCakeMass;
+        readonly fsCalculatorConstant m_dryCakeMass;
+        readonly fsCalculatorConstant m_c;
         readonly fsCalculatorVariable m_eps;
 
         public fsCakePorosityCalculator()
         {
             #region Parameters Initialization
 
-            m_area = AddVariable(fsParameterIdentifier.FilterArea);
-            m_diameter = AddVariable(fsParameterIdentifier.MachineDiameter);
+            m_machineArea = AddVariable(fsParameterIdentifier.FilterArea);
+            m_machineDiameter = AddVariable(fsParameterIdentifier.MachineDiameter);
+            m_filterElementDiameter = AddVariable(fsParameterIdentifier.FilterElementDiameter);
+            m_cakeArea = AddVariable(new fsParameterIdentifier("cakeArea"));
             m_b = AddVariable(fsParameterIdentifier.FilterB);
             m_bOverD = AddVariable(fsParameterIdentifier.FilterBOverDiameter);
             m_rhoL = AddConstant(fsParameterIdentifier.LiquidDensity);
             m_rhoS = AddConstant(fsParameterIdentifier.SolidsDensity);
-            m_hc = AddConstant(fsParameterIdentifier.CakeHeight);
-            m_Mcw = AddConstant(fsParameterIdentifier.WetCakeMass);
-            m_Mcd = AddConstant(fsParameterIdentifier.DryCakeMass);
-            m_C = AddConstant(fsParameterIdentifier.SolidsConcentration);
+            m_cakeHeight = AddConstant(fsParameterIdentifier.CakeHeight);
+            m_wetCakeMass = AddConstant(fsParameterIdentifier.WetCakeMass);
+            m_dryCakeMass = AddConstant(fsParameterIdentifier.DryCakeMass);
+            m_c = AddConstant(fsParameterIdentifier.SolidsConcentration);
             m_eps = AddVariable(fsParameterIdentifier.Porosity);
 
             #endregion
@@ -67,23 +71,33 @@ namespace StepCalculators
         public void RebuildEquationsList()
         {
             Equations = new List<fsCalculatorEquation>();
-            if (m_machineTypeOption == fsMachineTypeOption.ConcaveCylindric)
+
+            switch (m_machineTypeOption)
             {
-                AddEquation(new fsCylinderAreaEquation(m_area, m_diameter, m_b));
-                AddEquation(new fsProductEquation(m_b, m_diameter, m_bOverD));
+                case fsMachineTypeOption.PlainArea:
+                    AddEquation(new fsAssignEquation(m_cakeArea, m_machineArea));
+                    break;
+                case fsMachineTypeOption.ConvexCylindric:
+                    AddEquation(new fsConvexCakeAreaEquation(m_cakeArea, m_machineArea, m_cakeHeight, m_filterElementDiameter));
+                    break;
+                case fsMachineTypeOption.ConcaveCylindric:
+                    AddEquation(new fsConcaveCakeAreaEquation(m_cakeArea, m_machineArea, m_cakeHeight, m_machineDiameter));
+                    AddEquation(new fsCylinderAreaEquation(m_machineArea, m_machineDiameter, m_b));
+                    AddEquation(new fsProductEquation(m_b, m_machineDiameter, m_bOverD));
+                    break;
             }
 
             AddEquation(new fsPorosityEquation(
                             m_saltContentOption == fsSaltContentOption.Neglected,
                             m_saturationOption == fsSaturationOption.SaturatedCake,
                             m_eps,
-                            m_Mcd,
-                            m_Mcw,
+                            m_dryCakeMass,
+                            m_wetCakeMass,
                             m_rhoS,
                             m_rhoL,
-                            m_area,
-                            m_C,
-                            m_hc));
+                            m_cakeArea,
+                            m_c,
+                            m_cakeHeight));
         }
     }
 }
