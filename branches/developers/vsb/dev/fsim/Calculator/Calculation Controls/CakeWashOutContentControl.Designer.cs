26dcbb115bf5378586843858c467de62b48004bd
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2011-10-24 15:33:42 +0000

    cake wash content module

---
+++ branches/developers/vsb/dev/fsim/Calculator/Calculation Controls/CakeWashOutContentControl.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Calculator.Calculation_Controls
+{
+    sealed partial class CakeWashOutContentControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.washOutContentComboBox = new System.Windows.Forms.ComboBox();
+            this.fromComboBox = new System.Windows.Forms.ComboBox();
+            this.washOutContentLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGrid = new fmDataGrid.fmDataGrid();
+            this.ParameterColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValueColumn = new fmDataGrid.fmDataGridViewNumericalTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.washOutContentComboBox);
+            this.panel1.Controls.Add(this.fromComboBox);
+            this.panel1.Controls.Add(this.washOutContentLabel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 72);
+            this.panel1.TabIndex = 0;
+            // 
+            // washOutContentComboBox
+            // 
+            this.washOutContentComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.washOutContentComboBox.FormattingEnabled = true;
+            this.washOutContentComboBox.Location = new System.Drawing.Point(141, 35);
+            this.washOutContentComboBox.Name = "washOutContentComboBox";
+            this.washOutContentComboBox.Size = new System.Drawing.Size(136, 21);
+            this.washOutContentComboBox.TabIndex = 3;
+            // 
+            // fromComboBox
+            // 
+            this.fromComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.fromComboBox.FormattingEnabled = true;
+            this.fromComboBox.Location = new System.Drawing.Point(141, 3);
+            this.fromComboBox.Name = "fromComboBox";
+            this.fromComboBox.Size = new System.Drawing.Size(136, 21);
+            this.fromComboBox.TabIndex = 2;
+            // 
+            // washOutContentLabel
+            // 
+            this.washOutContentLabel.AutoSize = true;
+            this.washOutContentLabel.Location = new System.Drawing.Point(35, 39);
+            this.washOutContentLabel.Name = "washOutContentLabel";
+            this.washOutContentLabel.Size = new System.Drawing.Size(98, 13);
+            this.washOutContentLabel.TabIndex = 1;
+            this.washOutContentLabel.Text = "Wash Out Content:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGrid);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 72);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(280, 208);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.AllowUserToResizeRows = false;
+            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGrid.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ParameterColumn,
+            this.ValueColumn});
+            this.dataGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.dataGrid.HighLightCurrentRow = false;
+            this.dataGrid.Location = new System.Drawing.Point(8, 0);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowHeadersVisible = false;
+            this.dataGrid.RowTemplate.Height = 18;
+            this.dataGrid.Size = new System.Drawing.Size(272, 208);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // ParameterColumn
+            // 
+            this.ParameterColumn.HeaderText = "Parameter";
+            this.ParameterColumn.Name = "ParameterColumn";
+            this.ParameterColumn.ReadOnly = true;
+            this.ParameterColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ParameterColumn.Width = 180;
+            // 
+            // ValueColumn
+            // 
+            this.ValueColumn.HeaderText = "Value";
+            this.ValueColumn.Name = "ValueColumn";
+            this.ValueColumn.Width = 80;
+            // 
+            // CakeWashOutContentControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "CakeWashOutContentControl";
+            this.Size = new System.Drawing.Size(280, 280);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private fmDataGrid.fmDataGrid dataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ParameterColumn;
+        private fmDataGrid.fmDataGridViewNumericalTextBoxColumn ValueColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox washOutContentComboBox;
+        private System.Windows.Forms.ComboBox fromComboBox;
+        private System.Windows.Forms.Label washOutContentLabel;
+    }
+}
---
+++ branches/developers/vsb/dev/fsim/Calculator/Calculation Controls/CakeWashOutContentControl.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Parameters;
+using StepCalculators;
+
+namespace Calculator.Calculation_Controls
+{
+    public sealed partial class CakeWashOutContentControl : fsCalculatorControl
+    {
+        #region Calculation Option
+
+        private readonly fsCakeWashOutContentCalculator m_calculator = new fsCakeWashOutContentCalculator();
+
+        #endregion
+
+        public CakeWashOutContentControl()
+        {
+            InitializeComponent();
+
+            Calculators.Add(m_calculator);
+
+            var wetMassGroup = AddGroup(fsParameterIdentifier.WetCakeMass);
+            var dryMassGroup = AddGroup(fsParameterIdentifier.DryCakeMass);
+            var liquidGroup = AddGroup(fsParameterIdentifier.LiquidMass);
+            var cmGroup = AddGroup(fsParameterIdentifier.SolidsMassFraction);
+            var cGroup = AddGroup(fsParameterIdentifier.SolidsConcentration);
+            var rholGroup = AddGroup(fsParameterIdentifier.LiquidDensity);
+            var phGroup = AddGroup(fsParameterIdentifier.pH);
+            var outGroup = AddGroup(
+                fsParameterIdentifier.pHcake,
+                fsParameterIdentifier.CakeMoistureContent,
+                fsParameterIdentifier.CakeWashOutContent);
+
+            var groups = new[] {
+                wetMassGroup,
+                dryMassGroup,
+                liquidGroup,
+                cmGroup,
+                cGroup,
+                rholGroup,
+                phGroup,
+                outGroup
+            };
+
+            var colors = new[] {
+                Color.FromArgb(255, 255, 230),
+                Color.FromArgb(255, 230, 255)
+            };
+
+            for (int i = 0; i < groups.Length; ++i)
+            {
+                AddGroupToUI(dataGrid, groups[i], colors[i % colors.Length]);
+                SetGroupInput(groups[i], true);
+            }
+            SetGroupInput(outGroup, false);
+
+            fsMisc.FillList(fromComboBox.Items, typeof(fsCakeWashOutContentCalculator.fsFromCalculationOption));
+            EstablishCalculationOption(fsCakeWashOutContentCalculator.fsFromCalculationOption.WashOutConcentration);
+            AssignCalculationOptionAndControl(typeof(fsCakeWashOutContentCalculator.fsFromCalculationOption), fromComboBox);
+
+            fsMisc.FillList(washOutContentComboBox.Items, typeof(fsCakeWashOutContentCalculator.fsWashOutContentOption));
+            EstablishCalculationOption(fsCakeWashOutContentCalculator.fsWashOutContentOption.AsMassFraction);
+            AssignCalculationOptionAndControl(typeof(fsCakeWashOutContentCalculator.fsWashOutContentOption), washOutContentComboBox);
+
+            UpdateGroupsInputInfoFromCalculationOptions();
+            UpdateEquationsFromCalculationOptions();
+            Recalculate();
+            UpdateUIFromData();
+            ConnectUIWithDataUpdating(dataGrid,
+                fromComboBox,
+                washOutContentComboBox);
+        }
+
+        #region Routine Methods
+
+        protected override void UpdateGroupsInputInfoFromCalculationOptions()
+        {
+            // this control has only one calculation group -- RF
+        }
+
+        protected override void UpdateEquationsFromCalculationOptions()
+        {
+            m_calculator.FromCalculationOption =
+                (fsCakeWashOutContentCalculator.fsFromCalculationOption)
+                CalculationOptions[typeof(fsCakeWashOutContentCalculator.fsFromCalculationOption)];
+            m_calculator.WashOutContentOption =
+                (fsCakeWashOutContentCalculator.fsWashOutContentOption)
+                CalculationOptions[typeof(fsCakeWashOutContentCalculator.fsWashOutContentOption)];
+            m_calculator.RebuildEquationsList();
+        }
+
+        protected override void UpdateUIFromData()
+        {
+            var fromContentOption =
+                (fsCakeWashOutContentCalculator.fsFromCalculationOption)
+                CalculationOptions[typeof(fsCakeWashOutContentCalculator.fsFromCalculationOption)];
+            bool isFromWashOutConcentration = fromContentOption == fsCakeWashOutContentCalculator.fsFromCalculationOption.WashOutConcentration;
+
+            washOutContentLabel.Visible = isFromWashOutConcentration;
+            washOutContentComboBox.Visible = isFromWashOutConcentration;
+
+            var washOutContentOption =
+                (fsCakeWashOutContentCalculator.fsWashOutContentOption)
+                CalculationOptions[typeof(fsCakeWashOutContentCalculator.fsWashOutContentOption)];
+            bool isCmInput = washOutContentOption ==
+                             fsCakeWashOutContentCalculator.fsWashOutContentOption.AsMassFraction;
+
+            ParameterToCell[fsParameterIdentifier.SolidsMassFraction].OwningRow.Visible = isFromWashOutConcentration && isCmInput;
+            ParameterToCell[fsParameterIdentifier.SolidsConcentration].OwningRow.Visible = isFromWashOutConcentration && !isCmInput;
+            ParameterToCell[fsParameterIdentifier.LiquidDensity].OwningRow.Visible = !isFromWashOutConcentration || !isCmInput;
+            ParameterToCell[fsParameterIdentifier.pH].OwningRow.Visible = !isFromWashOutConcentration;
+            ParameterToCell[fsParameterIdentifier.pHcake].OwningRow.Visible = !isFromWashOutConcentration;
+
+            base.UpdateUIFromData();
+        }
+
+        #endregion
+    }
+}
