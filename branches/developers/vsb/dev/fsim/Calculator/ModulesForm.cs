93c947f82c9dcdbceb839f4e3e62bf4de34bc92d
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2011-10-27 19:21:49 +0000

    tree of modules

--- branches/developers/vsb/dev/fsim/Calculator/ModulesForm.cs
+++ branches/developers/vsb/dev/fsim/Calculator/ModulesForm.cs
@@ -25,7 +25,6 @@ namespace Calculator
 
         private void OkButtonClick(object sender, EventArgs e)
         {
-            SelectedModule = m_modules[listBox1.SelectedItem.ToString()];
             SelectedModule.Dock = DockStyle.None;
             DialogResult = DialogResult.OK;
             Close();
@@ -33,29 +32,58 @@ namespace Calculator
 
         private void ModulesFormLoad(object sender, EventArgs e)
         {
-            m_modules["Suspension Mass and Cake Height"] = new fsMsusAndHcControl();
-            m_modules["Cake Porosity"] = new fsCakePorossityControl();
-            m_modules["Densities and Solids Concentrations"] = new fsDensityConcentrationControl();
-            m_modules["Permeability and Cake Compressibility"] = new fsPermeabilityControl();
-            m_modules["Cake Moisture Content from Wet and Dry Cake Mass"] = new fsCakeMoistureContentFromWetAndDryCakeMassControl();
-            m_modules["Cake Moisture Content from Cake Saturation"] = new fsCakeMoistureContentFromCakeSaturationControl();
-            m_modules["Cake Wash Out Content"] = new fsCakeWashOutContentControl();
-            m_modules["Laboratory Filtration Time"] = new fsLaboratoryFiltrationTime();
 
-            foreach (var s in m_modules.Keys)
             {
-                listBox1.Items.Add(s);
+                TreeNode suspensionNode = new TreeNode("Suspension");
+                AddModuleToTree(suspensionNode, "Densities and Suspension Solids Content", new fsDensityConcentrationControl());
+                treeView1.Nodes.Add(suspensionNode);
             }
-            listBox1.SelectedIndex = 0;
+            {
+                TreeNode filterCakeNode = new TreeNode("Filter Cake");
+                AddModuleToTree(filterCakeNode, "Suspension Amount and Cake Height", new fsMsusAndHcControl());
+                AddModuleToTree(filterCakeNode, "Cake Porosity", new fsCakePorossityControl());
+                AddModuleToTree(filterCakeNode, "Cake Permeability/resistance and Cake Compressibility", new fsPermeabilityControl());
+                treeView1.Nodes.Add(filterCakeNode);
+            }
+            {
+                TreeNode cakeFormationNode = new TreeNode("Cake Formation");
+                AddModuleToTree(cakeFormationNode, "Calculation Cake Formation", new fsLaboratoryFiltrationTime());
+                treeView1.Nodes.Add(cakeFormationNode);
+            }
+            {
+                TreeNode cakeDeliquoringNode = new TreeNode("Cake Deliquoring");
+                AddModuleToTree(cakeDeliquoringNode, "Cake Moisture Content from Wet and Dry Cake Mass", new fsCakeMoistureContentFromWetAndDryCakeMassControl());
+                AddModuleToTree(cakeDeliquoringNode, "Cake Moisture Content from Cake Saturation", new fsCakeMoistureContentFromCakeSaturationControl());
+                treeView1.Nodes.Add(cakeDeliquoringNode);
+            }
+            {
+                TreeNode cakeWashingNode = new TreeNode("Cake Washing");
+                AddModuleToTree(cakeWashingNode, "Cake Wash Out Content X", new fsCakeWashOutContentControl());
+                treeView1.Nodes.Add(cakeWashingNode);
+            }
+
+            treeView1.ExpandAll();
         }
 
-        private void ListBox1SelectedIndexChanged(object sender, EventArgs e)
+        private void AddModuleToTree(TreeNode suspensionNode, string moduleName, fsCalculatorControl control)
         {
+            if (m_modules.ContainsKey(moduleName))
+                throw new Exception("Module with such name is already added.");
+            m_modules[moduleName] = control;
+            suspensionNode.Nodes.Add(moduleName);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            SelectedModuleName = treeView1.SelectedNode.Text;
+            if (!m_modules.ContainsKey(SelectedModuleName))
+                return;
+
             if (SelectedModule != null)
             {
                 SelectedModule.Parent = null;
             }
-            SelectedModuleName = listBox1.SelectedItem.ToString();
+            
             SelectedModule = m_modules[SelectedModuleName];
             SelectedModule.Parent = panel1;
             SelectedModule.Dock = DockStyle.Fill;
