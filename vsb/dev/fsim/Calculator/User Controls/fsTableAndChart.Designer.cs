8f2fdee51d7a22c92a53d2624467757749f7768d
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2011-12-06 17:28:47 +0000

    fsTableAndChart user control created

---
+++ vsb/dev/fsim/Calculator/User Controls/fsTableAndChart.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Calculator.User_Controls
+{
+    partial class fsTableAndChart
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.chartPanel = new System.Windows.Forms.Panel();
+            this.fmZedGraphControl1 = new fmZedGraph.fmZedGraphControl();
+            this.tablePanel = new System.Windows.Forms.Panel();
+            this.table = new fmDataGrid.fmDataGrid();
+            this.panel1.SuspendLayout();
+            this.chartPanel.SuspendLayout();
+            this.tablePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.checkedListBox1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(151, 251);
+            this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Y axis";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Detalization";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(71, 113);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(72, 124);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(71, 57);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(32, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Range";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(32, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X axis";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(71, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(72, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // chartPanel
+            // 
+            this.chartPanel.Controls.Add(this.fmZedGraphControl1);
+            this.chartPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartPanel.Location = new System.Drawing.Point(151, 0);
+            this.chartPanel.Name = "chartPanel";
+            this.chartPanel.Size = new System.Drawing.Size(304, 146);
+            this.chartPanel.TabIndex = 1;
+            // 
+            // fmZedGraphControl1
+            // 
+            this.fmZedGraphControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fmZedGraphControl1.IsAntiAlias = true;
+            this.fmZedGraphControl1.Location = new System.Drawing.Point(0, 0);
+            this.fmZedGraphControl1.Name = "fmZedGraphControl1";
+            this.fmZedGraphControl1.ScrollGrace = 0;
+            this.fmZedGraphControl1.ScrollMaxX = 0;
+            this.fmZedGraphControl1.ScrollMaxY = 0;
+            this.fmZedGraphControl1.ScrollMaxY2 = 0;
+            this.fmZedGraphControl1.ScrollMinX = 0;
+            this.fmZedGraphControl1.ScrollMinY = 0;
+            this.fmZedGraphControl1.ScrollMinY2 = 0;
+            this.fmZedGraphControl1.Size = new System.Drawing.Size(304, 146);
+            this.fmZedGraphControl1.TabIndex = 0;
+            // 
+            // tablePanel
+            // 
+            this.tablePanel.Controls.Add(this.table);
+            this.tablePanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tablePanel.Location = new System.Drawing.Point(151, 146);
+            this.tablePanel.Name = "tablePanel";
+            this.tablePanel.Size = new System.Drawing.Size(304, 105);
+            this.tablePanel.TabIndex = 2;
+            // 
+            // table
+            // 
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.table.HighLightCurrentRow = false;
+            this.table.Location = new System.Drawing.Point(0, 0);
+            this.table.Name = "table";
+            this.table.RowTemplate.Height = 18;
+            this.table.Size = new System.Drawing.Size(304, 105);
+            this.table.TabIndex = 0;
+            // 
+            // fsTableAndChart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.chartPanel);
+            this.Controls.Add(this.tablePanel);
+            this.Controls.Add(this.panel1);
+            this.Name = "fsTableAndChart";
+            this.Size = new System.Drawing.Size(455, 251);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.chartPanel.ResumeLayout(false);
+            this.tablePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel chartPanel;
+        private System.Windows.Forms.Panel tablePanel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private fmDataGrid.fmDataGrid table;
+        private fmZedGraph.fmZedGraphControl fmZedGraphControl1;
+    }
+}
