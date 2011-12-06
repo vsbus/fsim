e31def3ff8ad0d0acefa60ee2eec62ec7f94f4cb
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2011-12-06 17:28:47 +0000

    fsTableAndChart user control created

---
+++ branches/developers/vsb/dev/fsim/Calculator/User Controls/fsTableAndChart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator.User_Controls
+{
+    public partial class fsTableAndChart : UserControl
+    {
+        public fsTableAndChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
