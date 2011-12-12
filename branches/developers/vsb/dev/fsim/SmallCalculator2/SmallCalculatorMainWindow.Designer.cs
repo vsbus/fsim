333c5924ea125ba92e98f55f38addb5e67614028
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2011-12-12 11:33:09 +0000

    small calculator 2.0

---
+++ branches/developers/vsb/dev/fsim/SmallCalculator2/SmallCalculatorMainWindow.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SmallCalculator2
+{
+    partial class SmallCalculatorMainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new fsUIControls.fsTreeView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.modulePanel = new System.Windows.Forms.Panel();
+            this.currentModuleTitleLabel = new System.Windows.Forms.Label();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.modulePanel);
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(697, 519);
+            this.splitContainer1.SplitterDistance = 331;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(331, 519);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.currentModuleTitleLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(362, 43);
+            this.panel1.TabIndex = 0;
+            // 
+            // modulePanel
+            // 
+            this.modulePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.modulePanel.Location = new System.Drawing.Point(0, 43);
+            this.modulePanel.Name = "modulePanel";
+            this.modulePanel.Size = new System.Drawing.Size(362, 476);
+            this.modulePanel.TabIndex = 1;
+            // 
+            // currentModuleTitleLabel
+            // 
+            this.currentModuleTitleLabel.AutoSize = true;
+            this.currentModuleTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.currentModuleTitleLabel.Location = new System.Drawing.Point(3, 9);
+            this.currentModuleTitleLabel.Name = "currentModuleTitleLabel";
+            this.currentModuleTitleLabel.Size = new System.Drawing.Size(144, 13);
+            this.currentModuleTitleLabel.TabIndex = 0;
+            this.currentModuleTitleLabel.Text = "currentModuleTitleLabel";
+            // 
+            // SmallCalculatorMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 519);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "SmallCalculatorMainWindow";
+            this.Text = "Small Calculator 2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private fsUIControls.fsTreeView treeView1;
+        private System.Windows.Forms.Panel modulePanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label currentModuleTitleLabel;
+    }
+}
+
--- branches/developers/vsb/dev/fsim/fsUIControls/fsTreeView.cs
+++ branches/developers/vsb/dev/fsim/fsUIControls/fsTreeView.cs
@@ -2,7 +2,7 @@
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace Calculator
+namespace fsUIControls
 {
     public class fsTreeView : TreeView
     {
