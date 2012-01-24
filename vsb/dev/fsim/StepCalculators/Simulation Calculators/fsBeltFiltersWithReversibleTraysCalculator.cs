271b88b8fed0dc5de9b49066af594a4eef6572a4
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-01-24 20:49:06 +0000

    Belt Filters with Reversible Trays updated with one equation

---
+++ vsb/dev/fsim/StepCalculators/Simulation Calculators/fsBeltFiltersWithReversibleTraysCalculator.cs
@@ -0,0 +1,45 @@
+﻿using Equations.Belt_Filters_with_Reversible_Trays;
+using Parameters;
+
+namespace StepCalculators.Simulation_Calculators
+{
+    public class fsBeltFiltersWithReversibleTraysCalculator : fsCalculator
+    {
+        readonly fsCalculatorVariable m_filterArea;
+        readonly fsCalculatorVariable m_ns;
+        readonly fsCalculatorVariable m_Qms;
+        readonly fsCalculatorVariable m_lsOverB;
+        readonly fsCalculatorConstant m_rhoCs;
+        readonly fsCalculatorVariable m_u;
+        readonly fsCalculatorVariable m_cakeHeigth;
+        
+        public fsBeltFiltersWithReversibleTraysCalculator()
+        {
+            #region Parameters Initialization
+
+            m_filterArea = AddVariable(fsParameterIdentifier.FilterArea);
+            m_ns = AddVariable(fsParameterIdentifier.ns);
+            m_Qms = AddVariable(fsParameterIdentifier.Qms);
+            m_lsOverB = AddVariable(fsParameterIdentifier.ls_over_b);
+            m_u = AddVariable(fsParameterIdentifier.u);
+            m_cakeHeigth = AddVariable(fsParameterIdentifier.CakeHeight);
+
+            m_rhoCs = AddConstant(fsParameterIdentifier.DensityDryCake);
+
+            #endregion
+
+            #region Equations Initialization
+
+            Equations.Add(new fsAreaOfBeltWithReversibleTraysEquation(
+                m_filterArea,
+                m_lsOverB,
+                m_ns,
+                m_Qms,
+                m_rhoCs,
+                m_u,
+                m_cakeHeigth));
+
+            #endregion
+        }
+    }
+}
