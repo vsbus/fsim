8cf65f2b23130ef31dfbd4fcc1b8b5f7dc3d06dd
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-02-06 11:15:39 +0000

    Rf0 calculation in belt with reversible tray

---
+++ vsb/dev/fsim/StepCalculators/Simulation Calculators/Simulation Help Calculators/fsRf0Rs0RhoCw0FromDensitiesAndCakePorosity0Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Equations.Material.Cake_Moisture_Content_Rf_Equations;
+using Parameters;
+
+namespace StepCalculators.Simulation_Calculators.Simulation_Help_Calculators
+{
+    public class fsRf0Rs0RhoCw0FromDensitiesAndCakePorosity0Calculator : fsCalculator
+    {
+        readonly fsCalculatorConstant m_filtrateDensity;
+        readonly fsCalculatorConstant m_solidsDensity;
+        readonly fsCalculatorConstant m_cakePorosity0;
+        readonly fsCalculatorVariable m_cakeMoistureContentRf0;
+        readonly fsCalculatorVariable m_cakeWetMassSolidsFractionRs0;
+        readonly fsCalculatorVariable m_cakeWetDensity0;
+
+        public fsRf0Rs0RhoCw0FromDensitiesAndCakePorosity0Calculator()
+        {
+            #region Parameters Initialization
+
+            m_filtrateDensity = AddConstant(fsParameterIdentifier.FiltrateDensity);
+            m_solidsDensity = AddConstant(fsParameterIdentifier.SolidsDensity);
+            m_cakePorosity0 = AddConstant(fsParameterIdentifier.CakePorosity0);
+            m_cakeMoistureContentRf0 = AddVariable(fsParameterIdentifier.CakeMoistureContentRf0);
+            m_cakeWetMassSolidsFractionRs0 = AddVariable(fsParameterIdentifier.CakeWetMassSolidsFractionRs0);
+            m_cakeWetDensity0 = AddVariable(fsParameterIdentifier.CakeWetDensity0);
+
+            #endregion
+
+            #region Equations Initialization
+
+            Equations.Add(new fsMoistureContentFromDensitiesAndPorosityEquation(
+                m_cakeMoistureContentRf0,
+                m_cakePorosity0,
+                m_filtrateDensity,
+                m_solidsDensity));
+
+            #endregion
+        }
+    }
+}
