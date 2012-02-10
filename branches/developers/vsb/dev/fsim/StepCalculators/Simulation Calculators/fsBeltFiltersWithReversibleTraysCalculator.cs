5a11dc76721eab375656ae931722c524b97dd224
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-02-10 16:57:50 +0000

    belt filters update

--- branches/developers/vsb/dev/fsim/StepCalculators/Simulation Calculators/fsBeltFiltersWithReversibleTraysCalculator.cs
+++ branches/developers/vsb/dev/fsim/StepCalculators/Simulation Calculators/fsBeltFiltersWithReversibleTraysCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using Equations;
 using Equations.Belt_Filters_with_Reversible_Trays;
 using Parameters;
+using Value;
 
 namespace StepCalculators.Simulation_Calculators
 {
@@ -10,7 +11,6 @@ namespace StepCalculators.Simulation_Calculators
         {
             #region Parameters Initialization
 
-            IEquationParameter filterArea = AddVariable(fsParameterIdentifier.FilterArea);
             IEquationParameter ns = AddVariable(fsParameterIdentifier.ns);
             IEquationParameter Qms = AddVariable(fsParameterIdentifier.Qms);
             IEquationParameter Qs = AddVariable(fsParameterIdentifier.Qs);
@@ -31,6 +31,7 @@ namespace StepCalculators.Simulation_Calculators
             IEquationParameter tr = AddVariable(fsParameterIdentifier.ResidualTime);
 
             IEquationParameter cakeHeigth = AddVariable(fsParameterIdentifier.CakeHeight);
+            IEquationParameter filtrationTime = AddVariable(fsParameterIdentifier.FiltrationTime);
 
             IEquationParameter Dp = AddVariable(fsParameterIdentifier.PressureDifference);
 
@@ -48,36 +49,47 @@ namespace StepCalculators.Simulation_Calculators
             IEquationParameter ne = AddConstant(fsParameterIdentifier.Ne);
             IEquationParameter nc = AddConstant(fsParameterIdentifier.CakeCompressibility);
             IEquationParameter hce0 = AddConstant(fsParameterIdentifier.FilterMediumResistanceHce0);
-            IEquationParameter ttech = AddConstant(fsParameterIdentifier.ttech0);
+            IEquationParameter ttech0 = AddConstant(fsParameterIdentifier.ttech0);
+            IEquationParameter lambda = AddConstant(fsParameterIdentifier.lambda);
 
+            IEquationParameter ttech = AddVariable(fsParameterIdentifier.TechnicalTime);
+
+            IEquationParameter filterArea = AddVariable(fsParameterIdentifier.FilterArea);
+            IEquationParameter As = AddVariable(fsParameterIdentifier.As);
+            IEquationParameter filterLength = AddVariable(fsParameterIdentifier.FilterLength);
+            
+            var constantOne = new fsCalculatorConstant(new fsParameterIdentifier("1")) {Value = fsValue.One};
+            
             #endregion
 
             #region Equations Initialization
 
-            Equations.Add(new fsAreaOfBeltWithReversibleTraysEquation(
-                filterArea,
-                lsOverB,
-                ns,
-                Qms,
-                rho_s,
-                u,
-                cakeHeigth));
+            Equations.Add(new fsAreaOfBeltWithReversibleTraysEquation(filterArea, lsOverB, ns, Qms, rho_s, u, cakeHeigth));
 
             Equations.Add(new fsProductEquation(Qms, rho_s, Qs));
             Equations.Add(new fsProductEquation(Qmsus, rho_sus, Qsus));
             Equations.Add(new fsProductEquation(Qs, Qsus, cv));
-            Equations.Add(new fsProductsEquation(new IEquationParameter[] {ls, rho_cd0, u, cakeHeigth},
-                                                new IEquationParameter[] {lsOverB, Qms}));
+            Equations.Add(new fsProductsEquation(
+                new IEquationParameter[] {ls, rho_cd0, u, cakeHeigth},
+                new IEquationParameter[] {lsOverB, Qms}));
             Equations.Add(new fsProductEquation(lOverB, ns, lsOverB));
             Equations.Add(new fsProductEquation(nsf, ns, sf));
-
             Equations.Add(new fsFrom0AndDpEquation(eps, eps0, Dp, ne));
             Equations.Add(new fsFrom0AndDpEquation(Pc, Pc0, Dp, nc));
             Equations.Add(new fsEpsKappaCvEquation(eps, kappa, cv));
-
-            Equations.Add(new fsSfFromEtafHcHceKappaPcDpNsLsUTtechEquation(sf, etaf, cakeHeigth, hce0, kappa,
-                                                                           Pc, Dp, ns, ls, u, ttech));
-
+            Equations.Add(new fsTechnicalTimeFrom0Equation(ttech, ttech0, As, lambda));
+            Equations.Add(new fsProductsEquation(
+                new IEquationParameter[] { As, lsOverB },
+                new IEquationParameter[] { ls, ls }));
+            Equations.Add(new fsProductEquation(filterLength, ns, ls));
+            Equations.Add(new fsProductEquation(filterLength, tc, u));
+            Equations.Add(new fsDivisionInverseEquation(tc, n));
+            Equations.Add(new fsCakeHeightFromDpTf(cakeHeigth, hce0, Pc, kappa, Dp, filtrationTime, etaf));
+            Equations.Add(new fsSumEquation(ns, nsf, nsr));
+            Equations.Add(new fsSumEquation(constantOne, sr, sf));
+            Equations.Add(new fsProductEquation(tr, tc, sr));
+            Equations.Add(new fsSfFromEtafHcHceKappaPcDpNsLsUTtechEquation(
+                sf, etaf, cakeHeigth, hce0, kappa, Pc, Dp, ns, ls, u, ttech));
             #endregion
         }
     }
