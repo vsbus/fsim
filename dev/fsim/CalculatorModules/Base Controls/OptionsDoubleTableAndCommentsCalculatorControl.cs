a420608f1e709e0db1b7079e6a65bb4f6861af67
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-01 14:47:16 +0000

    create trunk as copy from vsb

---
+++ dev/fsim/CalculatorModules/Base Controls/OptionsDoubleTableAndCommentsCalculatorControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculatorModules.Base_Controls
+{
+    public partial class fsOptionsDoubleTableAndCommentsCalculatorControl : fsOptionsAndCommentsCalculatorControl
+    {
+        public fsOptionsDoubleTableAndCommentsCalculatorControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void StopGridsEdit()
+        {
+            dataGrid.EndEdit();
+            materialParametersDataGrid.EndEdit();
+        }
+    }
+}
---
+++ dev/fsim/CalculatorModules/PkeFromPcRcControl.cs
@@ -0,0 +1,128 @@
+﻿using System.Drawing;
+using CalculatorModules.Base_Controls;
+using Parameters;
+using StepCalculators;
+
+namespace CalculatorModules
+{
+    public sealed partial class fsPkeFromPcRcControl : fsOptionsSingleTableAndCommentsCalculatorControl
+    {
+        private readonly fsPkeFromPcRcCalculator m_calculator = new fsPkeFromPcRcCalculator();
+
+        public fsPkeFromPcRcControl()
+        {
+            InitializeComponent();
+
+            Calculators.Add(m_calculator);
+
+            fsParametersGroup permeabilityGroup = AddGroup(
+                fsParameterIdentifier.CakePermeability);
+            fsParametersGroup resistanceGroup = AddGroup(
+                fsParameterIdentifier.CakeResistance);
+            fsParametersGroup alphaGroup = AddGroup(
+                fsParameterIdentifier.CakeResistanceAlpha);
+            fsParametersGroup rhosGroup = AddGroup(
+                fsParameterIdentifier.SolidsDensity);
+            fsParametersGroup epsGroup = AddGroup(
+                fsParameterIdentifier.CakePorosity);
+            fsParametersGroup rhosBulkGroup = AddGroup(
+                fsParameterIdentifier.DryCakeDensity);
+            fsParametersGroup sigmaGroup = AddGroup(
+                fsParameterIdentifier.SurfaceTensionLiquidInCake);
+            fsParametersGroup pkestGroup = AddGroup(
+                fsParameterIdentifier.StandardCapillaryPressure);
+            fsParametersGroup pkeGroup = AddGroup(
+                fsParameterIdentifier.CapillaryPressure);
+
+            var groups = new[]
+                             {
+                                 permeabilityGroup,
+                                 resistanceGroup,
+                                 alphaGroup,
+                                 rhosGroup,
+                                 epsGroup,
+                                 rhosBulkGroup,
+                                 sigmaGroup,
+                                 pkestGroup,
+                                 pkeGroup
+                             };
+
+            var colors = new[]
+                             {
+                                 Color.FromArgb(255, 255, 230),
+                                 Color.FromArgb(255, 230, 255)
+                             };
+
+            for (int i = 0; i < groups.Length; ++i)
+            {
+                AddGroupToUI(dataGrid, groups[i], colors[i % colors.Length]);
+                SetGroupInput(groups[i], true);
+            }
+            SetGroupInput(pkeGroup, false);
+
+            fsMisc.FillList(inputCakeComboBox.Items, typeof (fsCalculationOptions.fsCakeInputOption));
+            AssignCalculationOptionAndControl(typeof (fsCalculationOptions.fsCakeInputOption), inputCakeComboBox);
+            EstablishCalculationOption(fsCalculationOptions.fsCakeInputOption.PermeabilityPc);
+
+            fsMisc.FillList(enterSolidsDensityComboBox.Items, typeof (fsCalculationOptions.fsEnterSolidsDensity));
+            AssignCalculationOptionAndControl(typeof (fsCalculationOptions.fsEnterSolidsDensity),
+                                              enterSolidsDensityComboBox);
+            EstablishCalculationOption(fsCalculationOptions.fsEnterSolidsDensity.BulkDensityDrySolids);
+
+            UpdateGroupsInputInfoFromCalculationOptions();
+            UpdateEquationsFromCalculationOptions();
+            Recalculate();
+            UpdateUIFromData();
+            ConnectUIWithDataUpdating(dataGrid,
+                                      inputCakeComboBox,
+                                      enterSolidsDensityComboBox);
+        }
+
+        #region Routine Methods
+
+        protected override void UpdateGroupsInputInfoFromCalculationOptions()
+        {
+            m_calculator.CakeInputOption =
+                (fsCalculationOptions.fsCakeInputOption)
+                CalculationOptions[typeof (fsCalculationOptions.fsCakeInputOption)];
+            m_calculator.EnterSolidsOption =
+                (fsCalculationOptions.fsEnterSolidsDensity)
+                CalculationOptions[typeof (fsCalculationOptions.fsEnterSolidsDensity)];
+            m_calculator.RebuildEquationsList();
+        }
+
+        protected override void UpdateEquationsFromCalculationOptions()
+        {
+            // this control uses only one calculator
+        }
+
+        protected override void UpdateUIFromData()
+        {
+            var cakeInputOption =
+                (fsCalculationOptions.fsCakeInputOption)
+                CalculationOptions[typeof (fsCalculationOptions.fsCakeInputOption)];
+            ParameterToCell[fsParameterIdentifier.CakePermeability].OwningRow.Visible =
+                cakeInputOption == fsCalculationOptions.fsCakeInputOption.PermeabilityPc;
+            ParameterToCell[fsParameterIdentifier.CakeResistance].OwningRow.Visible =
+                cakeInputOption == fsCalculationOptions.fsCakeInputOption.ResistanceRc;
+            ParameterToCell[fsParameterIdentifier.CakeResistanceAlpha].OwningRow.Visible =
+                cakeInputOption == fsCalculationOptions.fsCakeInputOption.ResistanceAlpha;
+
+            bool isAlpha = cakeInputOption == fsCalculationOptions.fsCakeInputOption.ResistanceAlpha;
+            enterSolidsDensityLabel.Visible = isAlpha;
+            enterSolidsDensityComboBox.Visible = isAlpha;
+
+            var enterSolidsDensityOption =
+                (fsCalculationOptions.fsEnterSolidsDensity)
+                CalculationOptions[typeof (fsCalculationOptions.fsEnterSolidsDensity)];
+            bool isBulk = enterSolidsDensityOption == fsCalculationOptions.fsEnterSolidsDensity.BulkDensityDrySolids;
+            ParameterToCell[fsParameterIdentifier.DryCakeDensity].OwningRow.Visible = isAlpha && isBulk;
+            ParameterToCell[fsParameterIdentifier.SolidsDensity].OwningRow.Visible = isAlpha && !isBulk;
+            ParameterToCell[fsParameterIdentifier.CakePorosity].OwningRow.Visible = isAlpha && !isBulk;
+
+            base.UpdateUIFromData();
+        }
+
+        #endregion
+    }
+}
