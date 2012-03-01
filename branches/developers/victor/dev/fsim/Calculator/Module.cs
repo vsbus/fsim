90a0b49860a9ad8fda2627d07795ef31a8afd2ab
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-01 14:58:49 +0000

    meaningless change

--- branches/developers/victor/dev/fsim/Calculator/Module.cs
+++ branches/developers/victor/dev/fsim/Calculator/Module.cs
@@ -18,11 +18,11 @@ namespace Calculator
             if (m_calculatorControl != null)
             {
                 Form = new Form
-                           {
-                               Text = Name,
-                               Width = m_calculatorControl.Width + 10,
-                               Height = m_calculatorControl.Height + 10
-                           };
+                        {
+                            Text = Name,
+                            Width = m_calculatorControl.Width + 10,
+                            Height = m_calculatorControl.Height + 10
+                        };
                 m_calculatorControl.Parent = Form;
                 m_calculatorControl.Dock = DockStyle.Fill;
                 Form.Show();
