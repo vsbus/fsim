7288b9823785553e78724c3f72c799931d1b9ec3
vitalik7888 <vitalik7888@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-02 10:58:51 +0000

    CurveTestBuilder(sample) added

---
+++ branches/developers/vitalik/dev/fsim/CurveTestBuilder/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CurveTestBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxGraficsChoose = new System.Windows.Forms.ListBox();
+            this.fmZedGraphControlMain = new fmZedGraph.fmZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // listBoxGraficsChoose
+            // 
+            this.listBoxGraficsChoose.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBoxGraficsChoose.FormattingEnabled = true;
+            this.listBoxGraficsChoose.ItemHeight = 16;
+            this.listBoxGraficsChoose.Items.AddRange(new object[] {
+            "sin(x)",
+            "sqrt(x)",
+            "x^2"});
+            this.listBoxGraficsChoose.Location = new System.Drawing.Point(0, 0);
+            this.listBoxGraficsChoose.Name = "listBoxGraficsChoose";
+            this.listBoxGraficsChoose.Size = new System.Drawing.Size(246, 468);
+            this.listBoxGraficsChoose.TabIndex = 1;
+            this.listBoxGraficsChoose.SelectedIndexChanged += new System.EventHandler(this.listBoxGraficsChoose_SelectedIndexChanged);
+            // 
+            // fmZedGraphControlMain
+            // 
+            this.fmZedGraphControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fmZedGraphControlMain.IsAntiAlias = true;
+            this.fmZedGraphControlMain.Location = new System.Drawing.Point(246, 0);
+            this.fmZedGraphControlMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.fmZedGraphControlMain.Name = "fmZedGraphControlMain";
+            this.fmZedGraphControlMain.ScrollGrace = 0;
+            this.fmZedGraphControlMain.ScrollMaxX = 0;
+            this.fmZedGraphControlMain.ScrollMaxY = 0;
+            this.fmZedGraphControlMain.ScrollMaxY2 = 0;
+            this.fmZedGraphControlMain.ScrollMinX = 0;
+            this.fmZedGraphControlMain.ScrollMinY = 0;
+            this.fmZedGraphControlMain.ScrollMinY2 = 0;
+            this.fmZedGraphControlMain.Size = new System.Drawing.Size(795, 482);
+            this.fmZedGraphControlMain.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 482);
+            this.Controls.Add(this.fmZedGraphControlMain);
+            this.Controls.Add(this.listBoxGraficsChoose);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxGraficsChoose;
+        private fmZedGraph.fmZedGraphControl fmZedGraphControlMain;
+    }
+}
+
---
+++ branches/developers/vitalik/dev/fsim/CurveTestBuilder/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace CurveTestBuilder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double sin(double x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+
+            return Math.Sin(x);
+        }
+
+        private double cos(double x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+
+            return Math.Cos(x);
+        }
+
+        private double sqrt(double x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+
+            return Math.Sqrt(x);
+        }
+
+        private void DrawGraph()
+        {
+            GraphPane pane = fmZedGraphControlMain.GraphPane;
+            pane.CurveList.Clear();
+            PointPairList list = new PointPairList();
+
+            double xmin = -50;
+            double xmax = 50;
+
+            for (double x = xmin; x <= xmax; x += 0.01)
+            {
+                if (listBoxGraficsChoose.SelectedIndex != -1)
+                {
+                    if (listBoxGraficsChoose.SelectedIndex == 0)
+                        list.Add(x, sin(x));
+                    if (listBoxGraficsChoose.SelectedIndex == 1)
+                        list.Add(x, cos(x));
+                    if (listBoxGraficsChoose.SelectedIndex == 2)
+                        list.Add(x, sqrt(x));
+                }
+            }
+
+            LineItem myCurve = pane.AddCurve("Sinc", list, Color.Blue, SymbolType.None);
+
+            fmZedGraphControlMain.AxisChange();
+
+            fmZedGraphControlMain.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxGraficsChoose_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DrawGraph();
+        }
+    }
+}
