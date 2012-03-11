8002922febd452485586aa9c482345c29ca198ff
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-11 22:10:02 +0000

    recreate victor's dev

---
+++ victor/dev/fsim/CalculatorModules/CakeMoistureContentFromWetAndDryCakeMassControl.cs
@@ -0,0 +1,111 @@
+﻿using System.Drawing;
+using CalculatorModules.Base_Controls;
+using Parameters;
+using StepCalculators;
+
+namespace CalculatorModules
+{
+    public sealed partial class fsCakeMoistureContentFromWetAndDryCakeMassControl :
+        fsOptionsSingleTableAndCommentsCalculatorControl
+    {
+        private readonly fsRfFromWetDryCakeCalculator m_calculator = new fsRfFromWetDryCakeCalculator();
+
+        public fsCakeMoistureContentFromWetAndDryCakeMassControl()
+        {
+            InitializeComponent();
+
+            Calculators.Add(m_calculator);
+
+            fsParametersGroup wetMassGroup = AddGroup(fsParameterIdentifier.WetCakeMass);
+            fsParametersGroup dryMassGroup = AddGroup(fsParameterIdentifier.DryCakeMass);
+            fsParametersGroup cmGroup = AddGroup(fsParameterIdentifier.SolutesMassFractionInLiquid);
+            fsParametersGroup cGroup = AddGroup(fsParameterIdentifier.SolutesConcentrationInCakeLiquid);
+            fsParametersGroup rholGroup = AddGroup(fsParameterIdentifier.LiquidDensity);
+            fsParametersGroup rfGroup = AddGroup(fsParameterIdentifier.CakeMoistureContent);
+
+            var groups = new[]
+                             {
+                                 wetMassGroup,
+                                 dryMassGroup,
+                                 cmGroup,
+                                 cGroup,
+                                 rholGroup,
+                                 rfGroup
+                             };
+
+            var colors = new[]
+                             {
+                                 Color.FromArgb(255, 255, 230),
+                                 Color.FromArgb(255, 230, 255)
+                             };
+
+            for (int i = 0; i < groups.Length; ++i)
+            {
+                AddGroupToUI(dataGrid, groups[i], colors[i % colors.Length]);
+                SetGroupInput(groups[i], true);
+            }
+            SetGroupInput(rfGroup, false);
+
+            fsMisc.FillList(saltContentComboBox.Items, typeof (fsCalculationOptions.fsSaltContentOption));
+            EstablishCalculationOption(fsCalculationOptions.fsSaltContentOption.Neglected);
+            AssignCalculationOptionAndControl(typeof (fsCalculationOptions.fsSaltContentOption), saltContentComboBox);
+
+            fsMisc.FillList(concentrationComboBox.Items, typeof (fsCalculationOptions.fsConcentrationOption));
+            EstablishCalculationOption(fsCalculationOptions.fsConcentrationOption.SolidsMassFraction);
+            AssignCalculationOptionAndControl(typeof (fsCalculationOptions.fsConcentrationOption), concentrationComboBox);
+
+            UpdateGroupsInputInfoFromCalculationOptions();
+            UpdateEquationsFromCalculationOptions();
+            Recalculate();
+            UpdateUIFromData();
+            ConnectUIWithDataUpdating(dataGrid,
+                                      saltContentComboBox,
+                                      concentrationComboBox);
+        }
+
+        #region Routine Methods
+
+        protected override void UpdateGroupsInputInfoFromCalculationOptions()
+        {
+            // this control has only one calculation group -- RF
+        }
+
+        protected override void UpdateEquationsFromCalculationOptions()
+        {
+            m_calculator.SaltContentOption =
+                (fsCalculationOptions.fsSaltContentOption)
+                CalculationOptions[typeof (fsCalculationOptions.fsSaltContentOption)];
+            m_calculator.ConcentrationOption =
+                (fsCalculationOptions.fsConcentrationOption)
+                CalculationOptions[typeof (fsCalculationOptions.fsConcentrationOption)];
+            m_calculator.RebuildEquationsList();
+        }
+
+        protected override void UpdateUIFromData()
+        {
+            var saltContentOption =
+                (fsCalculationOptions.fsSaltContentOption)
+                CalculationOptions[typeof (fsCalculationOptions.fsSaltContentOption)];
+            bool isSaltContConsidered = saltContentOption == fsCalculationOptions.fsSaltContentOption.Considered;
+
+            concentrationLabel.Visible = isSaltContConsidered;
+            concentrationComboBox.Visible = isSaltContConsidered;
+
+            var concentrationOption =
+                (fsCalculationOptions.fsConcentrationOption)
+                CalculationOptions[typeof (fsCalculationOptions.fsConcentrationOption)];
+            bool isCmInput = concentrationOption ==
+                             fsCalculationOptions.fsConcentrationOption.SolidsMassFraction;
+
+            ParameterToCell[fsParameterIdentifier.SolutesMassFractionInLiquid].OwningRow.Visible =
+                isSaltContConsidered && isCmInput;
+            ParameterToCell[fsParameterIdentifier.SolutesConcentrationInCakeLiquid].OwningRow.Visible =
+                isSaltContConsidered && !isCmInput;
+            ParameterToCell[fsParameterIdentifier.LiquidDensity].OwningRow.Visible = isSaltContConsidered && !isCmInput;
+
+            base.UpdateUIFromData();
+        }
+
+        #endregion
+    }
+}
---
+++ victor/dev/fsim/Equations/Material/Porosity Equations/fsPorositySaltConsideredCakeSaturatedEquation.cs
@@ -0,0 +1,55 @@
+﻿using Parameters;
+using Value;
+
+namespace Equations
+{
+    public class fsPorositySaltConsideredCakeSaturatedEquation : fsCalculatorEquation
+    {
+        #region Parameters
+
+        private readonly IEquationParameter m_dryMass;
+        private readonly IEquationParameter m_liquidDensity;
+        private readonly IEquationParameter m_porosity;
+        private readonly IEquationParameter m_solidsConcentration;
+        private readonly IEquationParameter m_solidsDensity;
+        private readonly IEquationParameter m_wetMass;
+
+        #endregion
+
+        public fsPorositySaltConsideredCakeSaturatedEquation(
+            IEquationParameter porosity,
+            IEquationParameter dryMass,
+            IEquationParameter wetMass,
+            IEquationParameter solidsConcentration,
+            IEquationParameter solidsDensity,
+            IEquationParameter liquidDensity)
+            : base(porosity, dryMass, wetMass, solidsConcentration, solidsDensity, liquidDensity)
+        {
+            m_porosity = porosity;
+            m_dryMass = dryMass;
+            m_wetMass = wetMass;
+            m_solidsConcentration = solidsConcentration;
+            m_solidsDensity = solidsDensity;
+            m_liquidDensity = liquidDensity;
+        }
+
+        protected override void InitFormulas()
+        {
+            AddFormula(m_porosity, PorosityFormula);
+        }
+
+        #region Formulas
+
+        private void PorosityFormula()
+        {
+            fsValue const0 = m_dryMass.Value -
+                             (m_wetMass.Value - m_dryMass.Value) * m_solidsConcentration.Value / m_liquidDensity.Value;
+            fsValue nom = const0;
+            fsValue const1 = m_solidsDensity.Value / m_liquidDensity.Value * (m_wetMass.Value - m_dryMass.Value);
+            fsValue den = const1 + const0;
+            m_porosity.Value = 1 - nom / den;
+        }
+
+        #endregion
+    }
+}
