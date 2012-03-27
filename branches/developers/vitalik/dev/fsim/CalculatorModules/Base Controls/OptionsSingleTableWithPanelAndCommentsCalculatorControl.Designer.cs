5e25651f8c89f7d8314e941ba74d1ef9ce319f74
vitalik7888 <vitalik7888@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-27 17:10:03 +0000

    comboBoxCalculationOption and label1 moved to Hydrocyclone/HydrocycloneControl changed label1 Text

--- branches/developers/vitalik/dev/fsim/CalculatorModules/Base Controls/OptionsSingleTableWithPanelAndCommentsCalculatorControl.Designer.cs
+++ branches/developers/vitalik/dev/fsim/CalculatorModules/Base Controls/OptionsSingleTableWithPanelAndCommentsCalculatorControl.Designer.cs
@@ -33,10 +33,7 @@
             this.ValueColumn = new fmDataGrid.fmDataGridViewNumericalTextBoxColumn();
             this.panelRight = new System.Windows.Forms.Panel();
             this.splitContainerMain = new System.Windows.Forms.SplitContainer();
-            this.comboBoxCalculationOption = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.leftTopPanel.SuspendLayout();
-            this.calculationOptionsPanel.SuspendLayout();
             this.tablesPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.fmDataGrid)).BeginInit();
             this.splitContainerMain.Panel1.SuspendLayout();
@@ -46,8 +43,6 @@
             // 
             // calculationOptionsPanel
             // 
-            this.calculationOptionsPanel.Controls.Add(this.comboBoxCalculationOption);
-            this.calculationOptionsPanel.Controls.Add(this.label1);
             this.calculationOptionsPanel.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             // 
             // tablesPanel
@@ -115,34 +110,12 @@
             this.splitContainerMain.SplitterDistance = 526;
             this.splitContainerMain.TabIndex = 2;
             // 
-            // comboBoxCalculationOption
-            // 
-            this.comboBoxCalculationOption.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxCalculationOption.FormattingEnabled = true;
-            this.comboBoxCalculationOption.Location = new System.Drawing.Point(112, 14);
-            this.comboBoxCalculationOption.Margin = new System.Windows.Forms.Padding(4);
-            this.comboBoxCalculationOption.Name = "comboBoxCalculationOption";
-            this.comboBoxCalculationOption.Size = new System.Drawing.Size(193, 24);
-            this.comboBoxCalculationOption.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 17);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
             // OptionsSingleTableWithPanelAndCommentsCalculatorControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "OptionsSingleTableWithPanelAndCommentsCalculatorControl";
             this.leftTopPanel.ResumeLayout(false);
-            this.calculationOptionsPanel.ResumeLayout(false);
-            this.calculationOptionsPanel.PerformLayout();
             this.tablesPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.fmDataGrid)).EndInit();
             this.splitContainerMain.Panel1.ResumeLayout(false);
@@ -158,8 +131,6 @@
         protected fmDataGrid.fmDataGrid fmDataGrid;
         private System.Windows.Forms.DataGridViewTextBoxColumn ParameterColumn;
         private fmDataGrid.fmDataGridViewNumericalTextBoxColumn ValueColumn;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBoxCalculationOption;
         private System.Windows.Forms.SplitContainer splitContainerMain;
     }
 }
--- branches/developers/vitalik/dev/fsim/CalculatorModules/Hydrocyclone/HydrocycloneControl.Designer.cs
+++ branches/developers/vitalik/dev/fsim/CalculatorModules/Hydrocyclone/HydrocycloneControl.Designer.cs
@@ -28,10 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.comboBoxCalculationOption = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.leftTopPanel.SuspendLayout();
+            this.calculationOptionsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // calculationOptionsPanel
+            // 
+            this.calculationOptionsPanel.Controls.Add(this.comboBoxCalculationOption);
+            this.calculationOptionsPanel.Controls.Add(this.label1);
+            // 
+            // comboBoxCalculationOption
+            // 
+            this.comboBoxCalculationOption.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCalculationOption.FormattingEnabled = true;
+            this.comboBoxCalculationOption.Location = new System.Drawing.Point(112, 14);
+            this.comboBoxCalculationOption.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxCalculationOption.Name = "comboBoxCalculationOption";
+            this.comboBoxCalculationOption.Size = new System.Drawing.Size(193, 24);
+            this.comboBoxCalculationOption.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Calculation:";
+            // 
+            // HydrocycloneControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "HydrocycloneControl";
+            this.leftTopPanel.ResumeLayout(false);
+            this.calculationOptionsPanel.ResumeLayout(false);
+            this.calculationOptionsPanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxCalculationOption;
+        private System.Windows.Forms.Label label1;
     }
 }
