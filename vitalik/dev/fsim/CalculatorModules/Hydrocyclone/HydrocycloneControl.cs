078aee30b8a40393fdfa36b0c3036cceac31d29d
vitalik7888 <vitalik7888@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-28 19:27:04 +0000

    added Region Groups to HydrocycloneControl

--- vitalik/dev/fsim/CalculatorModules/Hydrocyclone/HydrocycloneControl.cs
+++ vitalik/dev/fsim/CalculatorModules/Hydrocyclone/HydrocycloneControl.cs
@@ -6,14 +6,108 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Parameters;
+using StepCalculators;
+
 
 namespace CalculatorModules.Hydrocyclone
 {
     public partial class HydrocycloneControl : OptionsSingleTableWithPanelAndCommentsCalculatorControl
     {
+        #region Region Calculation Option
+
+        private enum fsRegionCalculationOption
+        {
+            [Description("Dp")]
+            Dp,
+            [Description("Q")]
+            Q,
+            [Description("n")]
+            n
+        }
+
+        #endregion
+
         public HydrocycloneControl()
         {
             InitializeComponent();
+
+            #region Region Calculators
+
+            // Calculators.Add(new fsRegionCalculators());
+
+            #endregion
+
+            var colors = new[]
+                             {
+                                 Color.FromArgb(255, 255, 230),
+                                 Color.FromArgb(255, 230, 255)
+                             };
+
+            #region region groups
+
+            fsParametersGroup etaGroup = AddGroup(
+               fsParameterIdentifier.LiquidViscosity);
+
+            fsParametersGroup rhoGroup = AddGroup(
+                fsParameterIdentifier.LiquidDensityRho);
+
+            fsParametersGroup densitiesGroup = AddGroup(
+                fsParameterIdentifier.SolidsDensity,
+                fsParameterIdentifier.SuspensionDensity);
+
+            fsParametersGroup cGroup = AddGroup(
+                fsParameterIdentifier.SuspensionSolidsMassFraction,
+                fsParameterIdentifier.SuspensionSolidsVolumeFraction,
+                fsParameterIdentifier.SuspensionSolidsConcentration);
+
+            var regionGroups = new[]
+                             {
+                                etaGroup,
+                                rhoGroup,
+                                densitiesGroup,
+                                cGroup,
+                             };
+
+            for (int i = 0; i < regionGroups.Length; ++i)
+            {
+                AddGroupToUI(fmDataGrid, regionGroups[i], colors[i % colors.Length]);
+                SetGroupInput(regionGroups[i], true);
+            }
+            
+
+            #endregion
+
+            fsMisc.FillList(comboBoxCalculationOption.Items, typeof(fsRegionCalculationOption));
+            EstablishCalculationOption(fsRegionCalculationOption.Dp);
+            AssignCalculationOptionAndControl(typeof(fsRegionCalculationOption), comboBoxCalculationOption);
+            UpdateUIFromData();
+            ConnectUIWithDataUpdating(fmDataGrid, comboBoxCalculationOption);
+
+        }
+
+        #region Routine Methods
+
+        protected override void UpdateGroupsInputInfoFromCalculationOptions()
+        {
+            //override
+        }
+
+        protected override void UpdateEquationsFromCalculationOptions()
+        {
+            //override
+        }
+
+        protected override void UpdateUIFromData()
+        {
+            //override
+
+            base.UpdateUIFromData();
+        }
+
+        #endregion
+
+
         }
     }
-}
+
