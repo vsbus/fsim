a5f126a02e3b6ecb890241bebc3cabb17eacbb83
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-29 18:59:30 +0000

    create test/dev

---
+++ test/dev/fsim/Equations/Cake Formation/fsFrom0AndDpEquation.cs
@@ -0,0 +1,66 @@
+﻿using Parameters;
+using Value;
+
+namespace Equations
+{
+    public class fsFrom0AndDpEquation : fsCalculatorEquation
+    {
+        #region Parameters
+
+        private readonly IEquationParameter m_degree;
+        private readonly IEquationParameter m_pressure;
+        private readonly IEquationParameter m_x;
+        private readonly IEquationParameter m_x0;
+
+        #endregion
+
+        public fsFrom0AndDpEquation(
+            IEquationParameter x,
+            IEquationParameter x0,
+            IEquationParameter pressure,
+            IEquationParameter degree)
+            : base(
+                x,
+                x0,
+                pressure,
+                degree)
+        {
+            m_x = x;
+            m_x0 = x0;
+            m_pressure = pressure;
+            m_degree = degree;
+        }
+
+        protected override void InitFormulas()
+        {
+            AddFormula(m_x, XFormula);
+            AddFormula(m_x0, X0Formula);
+            AddFormula(m_pressure, PressureFormula);
+            AddFormula(m_degree, DegreeFormula);
+        }
+
+        #region Formulas
+
+        private void XFormula()
+        {
+            m_x.Value = m_x0.Value * fsValue.Pow(m_pressure.Value / 1e5, -m_degree.Value);
+        }
+
+        private void X0Formula()
+        {
+            m_x0.Value = m_x.Value / fsValue.Pow(m_pressure.Value / 1e5, -m_degree.Value);
+        }
+
+        private void PressureFormula()
+        {
+            m_pressure.Value = 1e5 / fsValue.Pow(m_x.Value / m_x0.Value, 1 / m_degree.Value);
+        }
+
+        private void DegreeFormula()
+        {
+            m_degree.Value = fsValue.Log(m_x.Value / m_x0.Value) / fsValue.Log(1e5 / m_pressure.Value);
+        }
+
+        #endregion
+    }
+}
