e883d54a3ec9d61f553260c62e663441d50d68f3
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-29 20:41:18 +0000

    recreate victor/dev

---
+++ victor/dev/fsim/Calculator/Dialogs/ShowHideParametersDialog.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Calculator.Dialogs
+{
+    partial class fsShowHideParametersDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.fsParametersCheckBoxesTree1 = new fsUIControls.fsParametersCheckBoxesTree();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cancelButton);
+            this.panel1.Controls.Add(this.okButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 287);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 41);
+            this.panel1.TabIndex = 1;
+            // 
+            // CancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(223, 6);
+            this.cancelButton.Name = "CancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // OKButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.Location = new System.Drawing.Point(142, 6);
+            this.okButton.Name = "OKButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // fsParametersCheckBoxesTree1
+            // 
+            this.fsParametersCheckBoxesTree1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.fsParametersCheckBoxesTree1.Location = new System.Drawing.Point(12, 12);
+            this.fsParametersCheckBoxesTree1.Name = "fsParametersCheckBoxesTree1";
+            this.fsParametersCheckBoxesTree1.Size = new System.Drawing.Size(286, 269);
+            this.fsParametersCheckBoxesTree1.TabIndex = 3;
+            // 
+            // fsShowHideParametersDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 328);
+            this.Controls.Add(this.fsParametersCheckBoxesTree1);
+            this.Controls.Add(this.panel1);
+            this.Name = "fsShowHideParametersDialog";
+            this.Text = "ShowHideParametersDialog";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
+        private fsUIControls.fsParametersCheckBoxesTree fsParametersCheckBoxesTree1;
+    }
+}
