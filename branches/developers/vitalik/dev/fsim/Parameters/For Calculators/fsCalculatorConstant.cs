1fb57da33d4ba1de3db3b8fdd6775c3193a111fe
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-29 20:39:37 +0000

    recreate vitalik/dev

---
+++ branches/developers/vitalik/dev/fsim/Parameters/For Calculators/fsCalculatorConstant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Parameters
+{
+    public class fsCalculatorConstant : fsCalculatorParameter, IEquationParameter
+    {
+        public fsCalculatorConstant(fsParameterIdentifier identifier)
+            : base(identifier)
+        {
+        }
+
+        #region IEquationParameter Members
+
+        public bool IsProcessed
+        {
+            get { return true; }
+            set { throw new Exception("Set is processed is restricted for Consts"); }
+        }
+
+        #endregion
+    }
+}
---
+++ branches/developers/vitalik/dev/fsim/StepCalculators/Small Calculators/fsSuspensionSolidsMassFractionCalculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Parameters;
+using Equations;
+using Value;
+
+namespace StepCalculators
+{
+    public class fsSuspensionSolidsMassFractionCalculator : fsCalculator
+    {
+        readonly fsCalculatorConstant m_suspensionMass;
+        readonly fsCalculatorConstant m_dryMass;
+        readonly fsCalculatorConstant m_Cfm;
+        readonly fsCalculatorConstant m_Cf;
+        readonly fsCalculatorConstant m_motherLiquidDensity;
+        readonly fsCalculatorVariable m_Cm;
+        readonly fsCalculatorVariable m_internalC;
+
+        public fsSuspensionSolidsMassFractionCalculator()
+        {
+            #region Parameters Initialization
+
+            m_suspensionMass = AddConstant(fsParameterIdentifier.SuspensionMass);
+            m_dryMass = AddConstant(fsParameterIdentifier.DryCakeMass);
+            m_Cfm = AddConstant(fsParameterIdentifier.SolutesMassFractionInMotherLiquid);
+            m_Cf = AddConstant(fsParameterIdentifier.SolutesConcentrationInMotherLiquid);
+            m_motherLiquidDensity = AddConstant(fsParameterIdentifier.MotherLiquidDensity);
+            m_Cm = AddVariable(fsParameterIdentifier.SuspensionSolidsMassFraction);
+            m_internalC = AddVariable(new fsParameterIdentifier("internalC"));
+
+            #endregion
+
+            Equations = null;
+        }
+
+        public fsCalculationOptions.fsSaltContentOption SaltContentOption;
+        public fsCalculationOptions.fsConcentrationOption ConcentrationOption;
+
+        public void RebuildEquationsList()
+        {
+            Equations = new List<fsCalculatorEquation>();
+
+            if (SaltContentOption == fsCalculationOptions.fsSaltContentOption.Neglected)
+            {
+                m_internalC.Value = fsValue.Zero;
+                m_internalC.IsInput = true;
+                m_internalC.IsProcessed = true;
+                AddEquation(new fsSuspensionSolidsMassFractionEquation(m_Cm, m_dryMass, m_suspensionMass, m_internalC));
+            }
+            else
+            {
+                if (ConcentrationOption == fsCalculationOptions.fsConcentrationOption.SolidsMassFraction)
+                {
+                    AddEquation(new fsSuspensionSolidsMassFractionEquation(m_Cm, m_dryMass, m_suspensionMass, m_Cfm));
+                }
+                else
+                {
+                    m_internalC.IsInput = false;
+                    AddEquation(new fsProductEquation(m_Cf, m_internalC, m_motherLiquidDensity));
+                    AddEquation(new fsSuspensionSolidsMassFractionEquation(m_Cm, m_dryMass, m_suspensionMass, m_internalC));
+                }
+            }
+        }
+    }
+}
