a1853ac86c5bf589b46fc3645b3897f48e18abff
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-29 19:35:37 +0000

    create test/dev

---
+++ branches/developers/test/dev/fsim/Calculator/Section.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Calculator
+{
+    public class fsSection
+    {
+        private readonly List<fsModule> m_modules = new List<fsModule>();
+
+        public void AddModule(fsModule module)
+        {
+            m_modules.Add(module);
+        }
+
+        public void RemoveModule(fsModule module)
+        {
+            m_modules.Remove(module);
+        }
+    }
+}
