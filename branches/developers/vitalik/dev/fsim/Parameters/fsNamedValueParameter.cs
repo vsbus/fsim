238a12702ec368f0fd088eef94fbaa87ebbccffd
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-03-30 15:15:53 +0000

    recreate vitalik/dev

---
+++ branches/developers/vitalik/dev/fsim/Parameters/fsNamedValueParameter.cs
@@ -0,0 +1,35 @@
+﻿using Value;
+
+namespace Parameters
+{
+    public class fsNamedValueParameter : fsJustValueParameter
+    {
+        public fsParameterIdentifier Identifier { get; set; }
+
+        #region Constructors
+
+        public fsNamedValueParameter(fsNamedValueParameter other)
+            : base(other)
+        {
+            Identifier = other.Identifier;
+        }
+
+        public fsNamedValueParameter(fsParameterIdentifier identifier)
+        {
+            Identifier = identifier;
+        }
+
+        public fsNamedValueParameter(fsParameterIdentifier identifier, fsValue value)
+            : base(value)
+        {
+            Identifier = identifier;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return Identifier.Name + " = " + Value;
+        }
+    }
+}
