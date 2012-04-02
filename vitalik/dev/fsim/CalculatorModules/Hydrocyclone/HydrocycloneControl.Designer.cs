00bb62d8e8eb08c4b09c4deb029b87a2af4cc6f3
vitalik7888 <vitalik7888@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-04-02 08:44:35 +0000

    OptionsSingleTableWithPanelAndCommentsCalculatorControl.panelRight modifiers changed to Protected dataGridView_Result(1-3) removed from OptionsSingleTableWithPanelAndCommentsCalculatorControl to Hydrocyclone/HydrocycloneControl

--- vitalik/dev/fsim/CalculatorModules/Hydrocyclone/HydrocycloneControl.Designer.cs
+++ vitalik/dev/fsim/CalculatorModules/Hydrocyclone/HydrocycloneControl.Designer.cs
@@ -30,9 +30,33 @@
         {
             this.comboBoxCalculationOption = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView_Result3 = new System.Windows.Forms.DataGridView();
+            this.ParameterColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UnitsColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FeedColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UnderflowColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OverflowColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView_Result2 = new System.Windows.Forms.DataGridView();
+            this.ParameterColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UnitsColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValueColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView_Result1 = new System.Windows.Forms.DataGridView();
+            this.ParameterColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UnitsColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValueColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panelRight.SuspendLayout();
             this.leftTopPanel.SuspendLayout();
             this.calculationOptionsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Result3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Result2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Result1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelRight
+            // 
+            this.panelRight.Controls.Add(this.dataGridView_Result3);
+            this.panelRight.Controls.Add(this.dataGridView_Result2);
+            this.panelRight.Controls.Add(this.dataGridView_Result1);
             // 
             // calculationOptionsPanel
             // 
@@ -59,14 +83,165 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Calculation:";
             // 
+            // dataGridView_Result3
+            // 
+            this.dataGridView_Result3.AllowUserToAddRows = false;
+            this.dataGridView_Result3.AllowUserToDeleteRows = false;
+            this.dataGridView_Result3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Result3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ParameterColumn3,
+            this.UnitsColumn3,
+            this.FeedColumn,
+            this.UnderflowColumn,
+            this.OverflowColumn});
+            this.dataGridView_Result3.Location = new System.Drawing.Point(44, 163);
+            this.dataGridView_Result3.Name = "dataGridView_Result3";
+            this.dataGridView_Result3.ReadOnly = true;
+            this.dataGridView_Result3.RowTemplate.Height = 24;
+            this.dataGridView_Result3.Size = new System.Drawing.Size(433, 124);
+            this.dataGridView_Result3.TabIndex = 5;
+            // 
+            // ParameterColumn3
+            // 
+            this.ParameterColumn3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            this.ParameterColumn3.HeaderText = "Parameter";
+            this.ParameterColumn3.Name = "ParameterColumn3";
+            this.ParameterColumn3.ReadOnly = true;
+            this.ParameterColumn3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ParameterColumn3.Width = 5;
+            // 
+            // UnitsColumn3
+            // 
+            this.UnitsColumn3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            this.UnitsColumn3.HeaderText = "Units";
+            this.UnitsColumn3.Name = "UnitsColumn3";
+            this.UnitsColumn3.ReadOnly = true;
+            this.UnitsColumn3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.UnitsColumn3.Width = 5;
+            // 
+            // FeedColumn
+            // 
+            this.FeedColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            this.FeedColumn.HeaderText = "Feed";
+            this.FeedColumn.Name = "FeedColumn";
+            this.FeedColumn.ReadOnly = true;
+            this.FeedColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.FeedColumn.Width = 5;
+            // 
+            // UnderflowColumn
+            // 
+            this.UnderflowColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            this.UnderflowColumn.HeaderText = "Underflow";
+            this.UnderflowColumn.Name = "UnderflowColumn";
+            this.UnderflowColumn.ReadOnly = true;
+            this.UnderflowColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.UnderflowColumn.Width = 5;
+            // 
+            // OverflowColumn
+            // 
+            this.OverflowColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            this.OverflowColumn.HeaderText = "Overflow";
+            this.OverflowColumn.Name = "OverflowColumn";
+            this.OverflowColumn.ReadOnly = true;
+            this.OverflowColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.OverflowColumn.Width = 5;
+            // 
+            // dataGridView_Result2
+            // 
+            this.dataGridView_Result2.AllowUserToAddRows = false;
+            this.dataGridView_Result2.AllowUserToDeleteRows = false;
+            this.dataGridView_Result2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Result2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ParameterColumn2,
+            this.UnitsColumn2,
+            this.ValueColumn2});
+            this.dataGridView_Result2.Location = new System.Drawing.Point(266, 23);
+            this.dataGridView_Result2.Name = "dataGridView_Result2";
+            this.dataGridView_Result2.ReadOnly = true;
+            this.dataGridView_Result2.RowTemplate.Height = 24;
+            this.dataGridView_Result2.Size = new System.Drawing.Size(211, 124);
+            this.dataGridView_Result2.TabIndex = 4;
+            // 
+            // ParameterColumn2
+            // 
+            this.ParameterColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            this.ParameterColumn2.HeaderText = "Parameter";
+            this.ParameterColumn2.Name = "ParameterColumn2";
+            this.ParameterColumn2.ReadOnly = true;
+            this.ParameterColumn2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ParameterColumn2.Width = 5;
+            // 
+            // UnitsColumn2
+            // 
+            this.UnitsColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            this.UnitsColumn2.HeaderText = "Units";
+            this.UnitsColumn2.Name = "UnitsColumn2";
+            this.UnitsColumn2.ReadOnly = true;
+            this.UnitsColumn2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.UnitsColumn2.Width = 5;
+            // 
+            // ValueColumn2
+            // 
+            this.ValueColumn2.HeaderText = "Value";
+            this.ValueColumn2.Name = "ValueColumn2";
+            this.ValueColumn2.ReadOnly = true;
+            this.ValueColumn2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ValueColumn2.Width = 50;
+            // 
+            // dataGridView_Result1
+            // 
+            this.dataGridView_Result1.AllowUserToAddRows = false;
+            this.dataGridView_Result1.AllowUserToDeleteRows = false;
+            this.dataGridView_Result1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Result1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ParameterColumn1,
+            this.UnitsColumn1,
+            this.ValueColumn1});
+            this.dataGridView_Result1.Location = new System.Drawing.Point(44, 23);
+            this.dataGridView_Result1.Name = "dataGridView_Result1";
+            this.dataGridView_Result1.ReadOnly = true;
+            this.dataGridView_Result1.RowTemplate.Height = 24;
+            this.dataGridView_Result1.Size = new System.Drawing.Size(216, 124);
+            this.dataGridView_Result1.TabIndex = 3;
+            // 
+            // ParameterColumn1
+            // 
+            this.ParameterColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            this.ParameterColumn1.HeaderText = "Parameter";
+            this.ParameterColumn1.Name = "ParameterColumn1";
+            this.ParameterColumn1.ReadOnly = true;
+            this.ParameterColumn1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ParameterColumn1.Width = 5;
+            // 
+            // UnitsColumn1
+            // 
+            this.UnitsColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            this.UnitsColumn1.HeaderText = "Units";
+            this.UnitsColumn1.Name = "UnitsColumn1";
+            this.UnitsColumn1.ReadOnly = true;
+            this.UnitsColumn1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.UnitsColumn1.Width = 5;
+            // 
+            // ValueColumn1
+            // 
+            this.ValueColumn1.HeaderText = "Value";
+            this.ValueColumn1.Name = "ValueColumn1";
+            this.ValueColumn1.ReadOnly = true;
+            this.ValueColumn1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ValueColumn1.Width = 50;
+            // 
             // HydrocycloneControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "HydrocycloneControl";
+            this.panelRight.ResumeLayout(false);
             this.leftTopPanel.ResumeLayout(false);
             this.calculationOptionsPanel.ResumeLayout(false);
             this.calculationOptionsPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Result3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Result2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Result1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -75,5 +250,19 @@
 
         private System.Windows.Forms.ComboBox comboBoxCalculationOption;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView_Result3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ParameterColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UnitsColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FeedColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UnderflowColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OverflowColumn;
+        private System.Windows.Forms.DataGridView dataGridView_Result2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ParameterColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UnitsColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValueColumn2;
+        private System.Windows.Forms.DataGridView dataGridView_Result1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ParameterColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UnitsColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValueColumn1;
     }
 }
