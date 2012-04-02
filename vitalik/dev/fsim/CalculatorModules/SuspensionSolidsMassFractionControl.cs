22190c843ee8d6a82e2994fea3f38a71c766597d
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-04-02 14:36:20 +0000

    recreate vitalik/dev

---
+++ vitalik/dev/fsim/CalculatorModules/SuspensionSolidsMassFractionControl.cs
@@ -0,0 +1,112 @@
+﻿using System.Drawing;
+using CalculatorModules.Base_Controls;
+using Parameters;
+using StepCalculators;
+
+namespace CalculatorModules
+{
+    public partial class SuspensionSolidsMassFractionControl : fsOptionsSingleTableAndCommentsCalculatorControl
+    {
+        private readonly fsSuspensionSolidsMassFractionCalculator m_calculator =
+            new fsSuspensionSolidsMassFractionCalculator();
+
+        public SuspensionSolidsMassFractionControl()
+        {
+            InitializeComponent();
+
+            Calculators.Add(m_calculator);
+
+            fsParametersGroup wetMassGroup = AddGroup(fsParameterIdentifier.SuspensionMass);
+            fsParametersGroup dryMassGroup = AddGroup(fsParameterIdentifier.DryCakeMass);
+            fsParametersGroup cfmGroup = AddGroup(fsParameterIdentifier.SolutesMassFractionInMotherLiquid);
+            fsParametersGroup cfGroup = AddGroup(fsParameterIdentifier.SolutesConcentrationInMotherLiquid);
+            fsParametersGroup rhofGroup = AddGroup(fsParameterIdentifier.MotherLiquidDensity);
+            fsParametersGroup cmGroup = AddGroup(fsParameterIdentifier.SuspensionSolidsMassFraction);
+
+            var groups = new[]
+                             {
+                                 wetMassGroup,
+                                 dryMassGroup,
+                                 cfmGroup,
+                                 cfGroup,
+                                 rhofGroup,
+                                 cmGroup
+                             };
+
+            var colors = new[]
+                             {
+                                 Color.FromArgb(255, 255, 230),
+                                 Color.FromArgb(255, 230, 255)
+                             };
+
+            for (int i = 0; i < groups.Length; ++i)
+            {
+                AddGroupToUI(dataGrid, groups[i], colors[i % colors.Length]);
+                SetGroupInput(groups[i], true);
+            }
+            SetGroupInput(cmGroup, false);
+
+            fsMisc.FillList(saltContentComboBox.Items, typeof (fsCalculationOptions.fsSaltContentOption));
+            EstablishCalculationOption(fsCalculationOptions.fsSaltContentOption.Neglected);
+            AssignCalculationOptionAndControl(typeof (fsCalculationOptions.fsSaltContentOption), saltContentComboBox);
+
+            fsMisc.FillList(concentrationComboBox.Items, typeof (fsCalculationOptions.fsConcentrationOption));
+            EstablishCalculationOption(fsCalculationOptions.fsConcentrationOption.SolidsMassFraction);
+            AssignCalculationOptionAndControl(typeof (fsCalculationOptions.fsConcentrationOption), concentrationComboBox);
+
+            UpdateGroupsInputInfoFromCalculationOptions();
+            UpdateEquationsFromCalculationOptions();
+            Recalculate();
+            UpdateUIFromData();
+            ConnectUIWithDataUpdating(dataGrid,
+                                      saltContentComboBox,
+                                      concentrationComboBox);
+        }
+
+        #region Routine Methods
+
+        protected override void UpdateGroupsInputInfoFromCalculationOptions()
+        {
+            // this control has only one calculation group -- RF
+        }
+
+        protected override void UpdateEquationsFromCalculationOptions()
+        {
+            m_calculator.SaltContentOption =
+                (fsCalculationOptions.fsSaltContentOption)
+                CalculationOptions[typeof (fsCalculationOptions.fsSaltContentOption)];
+            m_calculator.ConcentrationOption =
+                (fsCalculationOptions.fsConcentrationOption)
+                CalculationOptions[typeof (fsCalculationOptions.fsConcentrationOption)];
+            m_calculator.RebuildEquationsList();
+        }
+
+        protected override void UpdateUIFromData()
+        {
+            var saltContentOption =
+                (fsCalculationOptions.fsSaltContentOption)
+                CalculationOptions[typeof (fsCalculationOptions.fsSaltContentOption)];
+            bool isSaltContConsidered = saltContentOption == fsCalculationOptions.fsSaltContentOption.Considered;
+
+            concentrationLabel.Visible = isSaltContConsidered;
+            concentrationComboBox.Visible = isSaltContConsidered;
+
+            var concentrationOption =
+                (fsCalculationOptions.fsConcentrationOption)
+                CalculationOptions[typeof (fsCalculationOptions.fsConcentrationOption)];
+            bool isCmInput = concentrationOption ==
+                             fsCalculationOptions.fsConcentrationOption.SolidsMassFraction;
+
+            ParameterToCell[fsParameterIdentifier.SolutesMassFractionInMotherLiquid].OwningRow.Visible =
+                isSaltContConsidered && isCmInput;
+            ParameterToCell[fsParameterIdentifier.SolutesConcentrationInMotherLiquid].OwningRow.Visible =
+                isSaltContConsidered && !isCmInput;
+            ParameterToCell[fsParameterIdentifier.MotherLiquidDensity].OwningRow.Visible = isSaltContConsidered &&
+                                                                                           !isCmInput;
+
+            base.UpdateUIFromData();
+        }
+
+        #endregion
+    }
+}
---
+++ vitalik/dev/fsim/Equations/Material/Densily Concentration Equations/Concentration Calculators/fsVolumeConcentrationEquation.cs
@@ -0,0 +1,78 @@
+﻿using Parameters;
+using Value;
+
+namespace Equations
+{
+    public class fsVolumeConcentrationEquation : fsCalculatorEquation
+    {
+        #region Parameters
+
+        private readonly IEquationParameter m_filtrateDensity;
+        private readonly IEquationParameter m_solidsDensity;
+        private readonly IEquationParameter m_solidsVolumeFractionConcentration;
+        private readonly IEquationParameter m_suspensionDensity;
+
+        #endregion
+
+        public fsVolumeConcentrationEquation(
+            IEquationParameter solidsVolumeFractionConcentration,
+            IEquationParameter filtrateDensity,
+            IEquationParameter solidsDensity,
+            IEquationParameter suspensionDensity)
+            : base(
+                solidsVolumeFractionConcentration,
+                filtrateDensity,
+                solidsDensity,
+                suspensionDensity)
+        {
+            m_solidsVolumeFractionConcentration = solidsVolumeFractionConcentration;
+            m_filtrateDensity = filtrateDensity;
+            m_solidsDensity = solidsDensity;
+            m_suspensionDensity = suspensionDensity;
+        }
+
+        protected override void InitFormulas()
+        {
+            AddFormula(m_solidsVolumeFractionConcentration, SolidsVolumeFractionFormula);
+            AddFormula(m_filtrateDensity, FiltrateDensityFormula);
+            AddFormula(m_solidsDensity, SolidsDensityFormula);
+            AddFormula(m_suspensionDensity, SuspensionDensityFormula);
+        }
+
+        #region Formulas
+
+        private void SolidsVolumeFractionFormula()
+        {
+            fsValue rhoF = m_filtrateDensity.Value;
+            fsValue rhoS = m_solidsDensity.Value;
+            fsValue rhoSus = m_suspensionDensity.Value;
+            m_solidsVolumeFractionConcentration.Value = (rhoF - rhoSus) / (rhoF - rhoS);
+        }
+
+        private void FiltrateDensityFormula()
+        {
+            fsValue cv = m_solidsVolumeFractionConcentration.Value;
+            fsValue rhoS = m_solidsDensity.Value;
+            fsValue rhoSus = m_suspensionDensity.Value;
+            m_filtrateDensity.Value = (rhoSus - cv * rhoS) / (1 - cv);
+        }
+
+        private void SolidsDensityFormula()
+        {
+            fsValue cv = m_solidsVolumeFractionConcentration.Value;
+            fsValue rhoF = m_filtrateDensity.Value;
+            fsValue rhoSus = m_suspensionDensity.Value;
+            m_solidsDensity.Value = (rhoSus - (1 - cv) * rhoF) / cv;
+        }
+
+        private void SuspensionDensityFormula()
+        {
+            fsValue cv = m_solidsVolumeFractionConcentration.Value;
+            fsValue rhoF = m_filtrateDensity.Value;
+            fsValue rhoS = m_solidsDensity.Value;
+            m_suspensionDensity.Value = rhoF * (1 - cv) + cv * rhoS;
+        }
+
+        #endregion
+    }
+}
