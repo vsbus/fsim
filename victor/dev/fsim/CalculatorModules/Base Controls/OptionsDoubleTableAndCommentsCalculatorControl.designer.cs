66f2c1187162b61e15c6a091b8dfe515a1750134
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-04-04 13:51:37 +0000

    recreate victor/dev

---
+++ victor/dev/fsim/CalculatorModules/Base Controls/OptionsDoubleTableAndCommentsCalculatorControl.designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CalculatorModules.Base_Controls
+{
+    partial class fsOptionsDoubleTableAndCommentsCalculatorControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tablesSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.dataGrid = new fsUIControls.fsParametersWithValuesTable();
+            this.materialParametersDataGrid = new fsUIControls.fsParametersWithValuesTable();
+            this.leftTopPanel.SuspendLayout();
+            this.tablesPanel.SuspendLayout();
+            this.tablesSplitContainer.Panel1.SuspendLayout();
+            this.tablesSplitContainer.Panel2.SuspendLayout();
+            this.tablesSplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // leftTopPanel
+            // 
+            this.leftTopPanel.Size = new System.Drawing.Size(365, 48);
+            // 
+            // calculationOptionsPanel
+            // 
+            this.calculationOptionsPanel.Size = new System.Drawing.Size(314, 48);
+            // 
+            // tablesPanel
+            // 
+            this.tablesPanel.Controls.Add(this.tablesSplitContainer);
+            this.tablesPanel.Size = new System.Drawing.Size(365, 357);
+            // 
+            // tablesSplitContainer
+            // 
+            this.tablesSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tablesSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.tablesSplitContainer.Name = "tablesSplitContainer";
+            // 
+            // tablesSplitContainer.Panel1
+            // 
+            this.tablesSplitContainer.Panel1.Controls.Add(this.materialParametersDataGrid);
+            // 
+            // tablesSplitContainer.Panel2
+            // 
+            this.tablesSplitContainer.Panel2.Controls.Add(this.dataGrid);
+            this.tablesSplitContainer.Size = new System.Drawing.Size(365, 357);
+            this.tablesSplitContainer.SplitterDistance = 178;
+            this.tablesSplitContainer.TabIndex = 1;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.dataGrid.Location = new System.Drawing.Point(0, 0);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(183, 357);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // materialParametersDataGrid
+            // 
+            this.materialParametersDataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialParametersDataGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.materialParametersDataGrid.Location = new System.Drawing.Point(0, 0);
+            this.materialParametersDataGrid.Name = "materialParametersDataGrid";
+            this.materialParametersDataGrid.Size = new System.Drawing.Size(178, 357);
+            this.materialParametersDataGrid.TabIndex = 0;
+            // 
+            // fsOptionsDoubleTableAndCommentsCalculatorControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "fsOptionsDoubleTableAndCommentsCalculatorControl";
+            this.Size = new System.Drawing.Size(365, 405);
+            this.leftTopPanel.ResumeLayout(false);
+            this.tablesPanel.ResumeLayout(false);
+            this.tablesSplitContainer.Panel1.ResumeLayout(false);
+            this.tablesSplitContainer.Panel2.ResumeLayout(false);
+            this.tablesSplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.SplitContainer tablesSplitContainer;
+        protected fsUIControls.fsParametersWithValuesTable dataGrid;
+        protected fsUIControls.fsParametersWithValuesTable materialParametersDataGrid;
+    }
+}
---
+++ victor/dev/fsim/StepCalculators/Small Calculators/fsMsusHcConcaveCylindricAreaCalculator.cs
@@ -0,0 +1,72 @@
+﻿using Equations;
+using Parameters;
+
+namespace StepCalculators
+{
+    public class fsMsusHcConcaveCylindricAreaCalculator : fsCalculator
+    {
+        readonly fsCalculatorVariable m_filtrateDensity;
+        readonly fsCalculatorVariable m_solidsDensity;
+        readonly fsCalculatorVariable m_suspensionDensity;
+        readonly fsCalculatorVariable m_solidsMassFraction;
+        readonly fsCalculatorVariable m_solidsVolumeFraction;
+        readonly fsCalculatorVariable m_solidsConcentration;
+
+        readonly fsCalculatorVariable m_porosity;
+        readonly fsCalculatorVariable m_kappa;
+        
+        readonly fsCalculatorVariable m_filterArea;
+        readonly fsCalculatorVariable m_filterDiameter;
+        readonly fsCalculatorVariable m_filterB;
+        readonly fsCalculatorVariable m_filterBOverDiameter;
+
+        readonly fsCalculatorVariable m_cakeHeight;
+
+        readonly fsCalculatorVariable m_suspensionMass;
+        readonly fsCalculatorVariable m_suspensionVolume;
+
+        public fsMsusHcConcaveCylindricAreaCalculator()
+        {
+            #region Parameters Initialization
+
+            m_filtrateDensity = AddVariable(fsParameterIdentifier.MotherLiquidDensity);
+            m_solidsDensity = AddVariable(fsParameterIdentifier.SolidsDensity);
+            m_suspensionDensity = AddVariable(fsParameterIdentifier.SuspensionDensity);
+            m_solidsMassFraction = AddVariable(fsParameterIdentifier.SuspensionSolidsMassFraction);
+            m_solidsVolumeFraction = AddVariable(fsParameterIdentifier.SuspensionSolidsVolumeFraction);
+            m_solidsConcentration = AddVariable(fsParameterIdentifier.SuspensionSolidsConcentration);
+
+            m_porosity = AddVariable(fsParameterIdentifier.CakePorosity);
+            m_kappa = AddVariable(fsParameterIdentifier.Kappa);
+            
+            m_filterArea = AddVariable(fsParameterIdentifier.FilterArea);
+            m_filterDiameter = AddVariable(fsParameterIdentifier.MachineDiameter);
+            m_filterB = AddVariable(fsParameterIdentifier.MachineWidth);
+            m_filterBOverDiameter = AddVariable(fsParameterIdentifier.WidthOverDiameterRatio);
+
+            m_cakeHeight = AddVariable(fsParameterIdentifier.CakeHeight);
+            m_suspensionMass = AddVariable(fsParameterIdentifier.SuspensionMass);
+            m_suspensionVolume = AddVariable(fsParameterIdentifier.SuspensionVolume);
+
+            #endregion
+
+            #region Equations Initialization
+
+            AddEquation(new fsMassConcentrationEquation(m_solidsMassFraction, m_filtrateDensity, m_solidsDensity, m_suspensionDensity));
+            AddEquation(new fsVolumeConcentrationEquation(m_solidsVolumeFraction, m_filtrateDensity, m_solidsDensity, m_suspensionDensity));
+            AddEquation(new fsConcentrationEquation(m_solidsConcentration, m_filtrateDensity, m_solidsDensity, m_suspensionDensity));
+
+            AddEquation(new fsEpsKappaCvEquation(m_porosity, m_kappa, m_solidsVolumeFraction));
+            AddEquation(new fsCylinderAreaEquation(m_filterArea, m_filterDiameter, m_filterB));
+            AddEquation(new fsProductEquation(m_filterB, m_filterDiameter, m_filterBOverDiameter));
+
+            AddEquation(new fsSuspensionMassFromHcEpsConcaveCylindircAreaEquation(m_suspensionMass, m_porosity, m_solidsDensity, m_filterArea, m_filterDiameter, m_cakeHeight, m_solidsMassFraction));
+            //AddEquation(new fsSuspensionVolumeFromHcEpsConvexCylindircAreaEquation(m_suspensionVolume, m_porosity, m_filterArea, m_cakeHeight, m_solidsVolumeFraction));
+            //AddEquation(new fsSuspensionVolumeFromHcKappaConvexCylindircAreaEquation(m_suspensionVolume, m_kappa, m_filterArea, m_cakeHeight));
+
+            AddEquation(new fsProductEquation(m_suspensionMass, m_suspensionVolume, m_suspensionDensity));
+            
+            #endregion
+        }
+    }
+}
