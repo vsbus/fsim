a33037ec3055045c15aaa5913f1447f6b91cdc4e
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-04-10 06:52:34 +0000

    create dmitry/dev

---
+++ dmitry/dev/fsim/Equations/Math/fsDivisionInverseEquation.cs
@@ -0,0 +1,45 @@
+﻿using Parameters;
+
+namespace Equations
+{
+    public class fsDivisionInverseEquation : fsCalculatorEquation
+    {
+        #region Parameters
+
+        private readonly IEquationParameter m_first;
+        private readonly IEquationParameter m_second;
+
+        #endregion
+
+        // first * second = 1
+
+        public fsDivisionInverseEquation(
+            IEquationParameter first,
+            IEquationParameter second)
+            : base(first, second)
+        {
+            m_first = first;
+            m_second = second;
+        }
+
+        protected override void InitFormulas()
+        {
+            AddFormula(m_first, FirstFormula);
+            AddFormula(m_second, SecondFormula);
+        }
+
+        #region Formulas
+
+        private void FirstFormula()
+        {
+            m_first.Value = 1 / m_second.Value;
+        }
+
+        private void SecondFormula()
+        {
+            m_second.Value = 1 / m_first.Value;
+        }
+
+        #endregion
+    }
+}
