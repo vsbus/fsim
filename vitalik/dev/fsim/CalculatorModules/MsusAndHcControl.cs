132ec489d410d3a40f8c0fc1fabd36e0c203787f
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-05-05 10:13:38 +0000

    recreate vitalik/dev

---
+++ vitalik/dev/fsim/CalculatorModules/MsusAndHcControl.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using CalculatorModules.Base_Controls;
+using Parameters;
+using StepCalculators;
+
+namespace CalculatorModules
+{
+    public sealed partial class fsMsusAndHcControl : fsOptionsSingleTableAndCommentsCalculatorControl
+    {
+        #region Calculation Data
+
+        private enum fsCalculationOption
+        {
+            [Description("Densities")] DenisitiesCalculated,
+            [Description("Concentrations")] ConcentreationsCalculated,
+            [Description("Porosity / Kappa")] PorosityKappaCalculated,
+            [Description("Machine Diameter")] MachineDiameterCalculated,
+            [Description("Filter Element Diameter")] FilterElementDiameterCalculated,
+            [Description("Machine Geometry")] MachineAreaBCalculated,
+            [Description("Cake Height")] CakeHeightCalculated,
+            [Description("Mass / Volume")] MassVolumeCalculated
+        }
+
+        #endregion
+
+        #region Routine Data
+
+        private readonly fsParametersGroup m_areaBGroup;
+
+        private readonly List<fsCalculator> m_concaveCylindricAreaCalculators = new List<fsCalculator>();
+        private readonly List<fsCalculator> m_convexCylindricAreaCalculators = new List<fsCalculator>();
+        private readonly List<fsCalculator> m_plainAreaCalculators = new List<fsCalculator>();
+
+        #endregion
+
+        public fsMsusAndHcControl()
+        {
+            InitializeComponent();
+
+            m_plainAreaCalculators.Add(new fsMsusHcPlainAreaCalculator());
+            m_convexCylindricAreaCalculators.Add(new fsMsusHcConvexCylindricAreaCalculator());
+            m_concaveCylindricAreaCalculators.Add(new fsMsusHcConcaveCylindricAreaCalculator());
+            Calculators = m_plainAreaCalculators;
+
+            fsParametersGroup filtrateGroup = AddGroup(
+                fsParameterIdentifier.MotherLiquidDensity);
+            fsParametersGroup densitiesGroup = AddGroup(
+                fsParameterIdentifier.SolidsDensity,
+                fsParameterIdentifier.SuspensionDensity);
+            fsParametersGroup concentrationGroup = AddGroup(
+                fsParameterIdentifier.SuspensionSolidsMassFraction,
+                fsParameterIdentifier.SuspensionSolidsVolumeFraction,
+                fsParameterIdentifier.SuspensionSolidsConcentration);
+            fsParametersGroup epsKappaGroup = AddGroup(
+                fsParameterIdentifier.CakePorosity,
+                fsParameterIdentifier.Kappa);
+            fsParametersGroup machineDiameterGroup = AddGroup(
+                fsParameterIdentifier.MachineDiameter);
+            m_areaBGroup = AddGroup(
+                fsParameterIdentifier.MachineWidth,
+                fsParameterIdentifier.WidthOverDiameterRatio,
+                fsParameterIdentifier.FilterArea);
+            fsParametersGroup diameterFilterElementGroup = AddGroup(
+                fsParameterIdentifier.FilterElementDiameter);
+            fsParametersGroup cakeHeightGroup = AddGroup(
+                fsParameterIdentifier.CakeHeight);
+            fsParametersGroup massVolumeGroup = AddGroup(
+                fsParameterIdentifier.SuspensionVolume,
+                fsParameterIdentifier.SuspensionMass);
+
+            var groups = new[]
+                             {
+                                 filtrateGroup,
+                                 densitiesGroup,
+                                 concentrationGroup,
+                                 epsKappaGroup,
+                                 machineDiameterGroup,
+                                 m_areaBGroup,
+                                 diameterFilterElementGroup,
+                                 cakeHeightGroup,
+                                 massVolumeGroup
+                             };
+
+            var colors = new[]
+                             {
+                                 Color.FromArgb(255, 255, 230),
+                                 Color.FromArgb(255, 230, 255)
+                             };
+
+            for (int i = 0; i < groups.Length; ++i)
+            {
+                AddGroupToUI(dataGrid, groups[i], colors[i % colors.Length]);
+            }
+            SetRowColor(dataGrid, ParameterToCell[fsParameterIdentifier.FilterArea].RowIndex,
+                        Color.FromArgb(255, 230, 230));
+            SetRowColor(dataGrid, ParameterToCell[fsParameterIdentifier.MachineWidth].RowIndex,
+                        Color.FromArgb(255, 230, 230));
+            SetRowColor(dataGrid, ParameterToCell[fsParameterIdentifier.WidthOverDiameterRatio].RowIndex,
+                        Color.FromArgb(255, 230, 230));
+
+            fsMisc.FillList(machineTypeComboBox.Items, typeof (fsCakePorosityCalculator.fsMachineTypeOption));
+            EstablishCalculationOption(fsCakePorosityCalculator.fsMachineTypeOption.PlainArea);
+            AssignCalculationOptionAndControl(typeof (fsCakePorosityCalculator.fsMachineTypeOption), machineTypeComboBox);
+
+            EstablishCalculationOption(fsCalculationOption.MassVolumeCalculated);
+            FillCalculationComboBox();
+
+            m_isBlockedCalculationOptionChanged = false;
+            AssignCalculationOptionAndControl(typeof (fsCalculationOption), calculationOptionComboBox);
+
+            UpdateGroupsInputInfoFromCalculationOptions();
+            UpdateEquationsFromCalculationOptions();
+            Recalculate();
+            UpdateUIFromData();
+            ConnectUIWithDataUpdating(dataGrid,
+                                      machineTypeComboBox,
+                                      calculationOptionComboBox);
+        }
+
+        #region Routine Methods
+
+        private bool m_isBlockedCalculationOptionChanged;
+
+        private void FillCalculationComboBox()
+        {
+            var machineTypeOption =
+                (fsCakePorosityCalculator.fsMachineTypeOption)
+                CalculationOptions[typeof (fsCakePorosityCalculator.fsMachineTypeOption)];
+            var restrictedOptions = new List<fsCalculationOption>();
+            if (machineTypeOption == fsCakePorosityCalculator.fsMachineTypeOption.PlainArea)
+            {
+                restrictedOptions.Add(fsCalculationOption.MachineDiameterCalculated);
+                restrictedOptions.Add(fsCalculationOption.FilterElementDiameterCalculated);
+            }
+            if (machineTypeOption == fsCakePorosityCalculator.fsMachineTypeOption.ConvexCylindric)
+            {
+                restrictedOptions.Add(fsCalculationOption.MachineDiameterCalculated);
+            }
+            if (machineTypeOption == fsCakePorosityCalculator.fsMachineTypeOption.ConcaveCylindric)
+            {
+                restrictedOptions.Add(fsCalculationOption.FilterElementDiameterCalculated);
+            }
+
+            var calculationOption = (fsCalculationOption) CalculationOptions[typeof (fsCalculationOption)];
+            if (restrictedOptions.Contains(calculationOption))
+            {
+                EstablishCalculationOption(fsCalculationOption.MassVolumeCalculated);
+            }
+
+            fsMisc.FillList(calculationOptionComboBox.Items, typeof (fsCalculationOption));
+            foreach (fsCalculationOption restrictedOption in restrictedOptions)
+            {
+                calculationOptionComboBox.Items.Remove(fsMisc.GetEnumDescription(restrictedOption));
+            }
+            calculationOptionComboBox.Text =
+                fsMisc.GetEnumDescription((fsCalculationOption) CalculationOptions[typeof (fsCalculationOption)]);
+        }
+
+        protected override void UpdateGroupsInputInfoFromCalculationOptions()
+        {
+            var calculationOption = (fsCalculationOption) CalculationOptions[typeof (fsCalculationOption)];
+            fsParametersGroup calculateGroup = null;
+            switch (calculationOption)
+            {
+                case fsCalculationOption.DenisitiesCalculated:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.SolidsDensity];
+                    break;
+                case fsCalculationOption.ConcentreationsCalculated:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.SuspensionSolidsMassFraction];
+                    break;
+                case fsCalculationOption.PorosityKappaCalculated:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.CakePorosity];
+                    break;
+                case fsCalculationOption.MachineDiameterCalculated:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.MachineDiameter];
+                    break;
+                case fsCalculationOption.FilterElementDiameterCalculated:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.FilterElementDiameter];
+                    break;
+                case fsCalculationOption.MachineAreaBCalculated:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.FilterArea];
+                    break;
+                case fsCalculationOption.CakeHeightCalculated:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.CakeHeight];
+                    break;
+                case fsCalculationOption.MassVolumeCalculated:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.SuspensionMass];
+                    break;
+            }
+            foreach (fsParametersGroup group in ParameterToGroup.Values)
+            {
+                SetGroupInput(group, group != calculateGroup);
+            }
+        }
+
+        protected override void CalculationOptionChanged(object sender, EventArgs e)
+        {
+            if (m_isBlockedCalculationOptionChanged)
+                return;
+
+            m_isBlockedCalculationOptionChanged = true;
+
+            UpdateCalculationOptionFromUI();
+            FillCalculationComboBox();
+
+            m_isBlockedCalculationOptionChanged = false;
+
+            base.CalculationOptionChanged(sender, e);
+        }
+
+        protected override void UpdateUIFromData()
+        {
+            var machineTypeOption =
+                (fsCakePorosityCalculator.fsMachineTypeOption)
+                CalculationOptions[typeof (fsCakePorosityCalculator.fsMachineTypeOption)];
+            if (machineTypeOption == fsCakePorosityCalculator.fsMachineTypeOption.PlainArea)
+            {
+                ParameterToCell[fsParameterIdentifier.MachineDiameter].OwningRow.Visible = false;
+                ParameterToCell[fsParameterIdentifier.FilterElementDiameter].OwningRow.Visible = false;
+                ParameterToCell[fsParameterIdentifier.MachineWidth].OwningRow.Visible = false;
+                ParameterToCell[fsParameterIdentifier.WidthOverDiameterRatio].OwningRow.Visible = false;
+            }
+            if (machineTypeOption == fsCakePorosityCalculator.fsMachineTypeOption.ConvexCylindric)
+            {
+                ParameterToCell[fsParameterIdentifier.MachineDiameter].OwningRow.Visible = false;
+                ParameterToCell[fsParameterIdentifier.FilterElementDiameter].OwningRow.Visible = true;
+                ParameterToCell[fsParameterIdentifier.MachineWidth].OwningRow.Visible = false;
+                ParameterToCell[fsParameterIdentifier.WidthOverDiameterRatio].OwningRow.Visible = false;
+            }
+            if (machineTypeOption == fsCakePorosityCalculator.fsMachineTypeOption.ConcaveCylindric)
+            {
+                ParameterToCell[fsParameterIdentifier.MachineDiameter].OwningRow.Visible = true;
+                ParameterToCell[fsParameterIdentifier.FilterElementDiameter].OwningRow.Visible = false;
+                ParameterToCell[fsParameterIdentifier.MachineWidth].OwningRow.Visible = true;
+                ParameterToCell[fsParameterIdentifier.WidthOverDiameterRatio].OwningRow.Visible = true;
+            }
+
+            base.UpdateUIFromData();
+        }
+
+        protected override void UpdateEquationsFromCalculationOptions()
+        {
+            var machineTypeOption =
+                (fsCakePorosityCalculator.fsMachineTypeOption)
+                CalculationOptions[typeof (fsCakePorosityCalculator.fsMachineTypeOption)];
+            if (machineTypeOption == fsCakePorosityCalculator.fsMachineTypeOption.PlainArea)
+            {
+                Calculators = m_plainAreaCalculators;
+                m_areaBGroup.Representator = fsParameterIdentifier.FilterArea;
+            }
+            if (machineTypeOption == fsCakePorosityCalculator.fsMachineTypeOption.ConvexCylindric)
+            {
+                Calculators = m_convexCylindricAreaCalculators;
+                m_areaBGroup.Representator = fsParameterIdentifier.FilterArea;
+            }
+            if (machineTypeOption == fsCakePorosityCalculator.fsMachineTypeOption.ConcaveCylindric)
+            {
+                Calculators = m_concaveCylindricAreaCalculators;
+                m_areaBGroup.Representator = fsParameterIdentifier.FilterArea;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ vitalik/dev/fsim/CalculatorModules/User Controls/fsDiagramWithTable.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Value;
+using ZedGraph;
+
+namespace CalculatorModules.User_Controls
+{
+    public partial class fsDiagramWithTable : UserControl
+    {
+        private readonly List<fsNamedArray> m_yAxis = new List<fsNamedArray>();
+        private readonly List<fsNamedArray> m_y2Axis = new List<fsNamedArray>();
+        private fsNamedArray m_xAxis;
+
+        public fsDiagramWithTable()
+        {
+            InitializeComponent();
+        }
+
+        public void SetXAxis(fsNamedArray xAxis)
+        {
+            m_xAxis = xAxis;
+        }
+
+        public void ClearYAxis()
+        {
+            m_yAxis.Clear();
+        }
+
+        public void AddYAxis(fsNamedArray yAxis)
+        {
+            m_yAxis.Add(yAxis);
+        }
+
+        public void ClearY2Axis()
+        {
+            m_y2Axis.Clear();
+        }
+
+        public void AddY2Axis(fsNamedArray yAxis)
+        {
+            m_y2Axis.Add(yAxis);
+        }
+
+        public void Redraw()
+        {
+            RefreshCurves();
+            RefreshTable();
+        }
+
+        private void RefreshTable()
+        {
+            table.ColumnCount = 0;
+            AddArrayToTable(m_xAxis);
+            foreach (fsNamedArray curve in m_yAxis)
+            {
+                AddArrayToTable(curve);
+            }
+            foreach (fsNamedArray curve in m_y2Axis)
+            {
+                AddArrayToTable(curve);
+            }
+        }
+
+        private void AddArrayToTable(fsNamedArray curve)
+        {
+            table.ColumnCount++;
+            table.RowCount = Math.Max(table.RowCount, curve.Array.Length);
+            DataGridViewColumn column = table.Columns[table.ColumnCount - 1];
+            column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            column.HeaderCell.Value = curve.Name;
+            for (int row = 0; row < curve.Array.Length; ++row)
+            {
+                table[table.ColumnCount - 1, row].Value = curve.Array[row].ToString();
+            }
+        }
+
+        private void RefreshCurves()
+        {
+            fmZedGraphControl1.GraphPane.CurveList.Clear();
+            fmZedGraphControl1.GraphPane.XAxis.Title.Text = m_xAxis.Name;
+            fmZedGraphControl1.GraphPane.YAxis.IsVisible = false;
+            fmZedGraphControl1.GraphPane.Y2Axis.IsVisible = false;
+
+            RefreshYAxis(m_yAxis, false);
+            RefreshYAxis(m_y2Axis, true);
+
+            fmZedGraphControl1.GraphPane.AxisChange();
+            fmZedGraphControl1.Refresh();
+        }
+
+        private void RefreshYAxis(List<fsNamedArray> yAxis, bool isY2Axis)
+        {
+            if (yAxis.Count > 0)
+            {
+                Color color;
+                Axis graphYAxis;
+                if (isY2Axis)
+                {
+                    color = Color.Green;
+                    graphYAxis = fmZedGraphControl1.GraphPane.Y2Axis;
+                }
+                else
+                {
+                    color = Color.Blue;
+                    graphYAxis = fmZedGraphControl1.GraphPane.YAxis;
+                }
+                graphYAxis.IsVisible = true;
+                graphYAxis.Color = color;
+                graphYAxis.Title.Text = "";
+
+                var maxValues = new double[yAxis.Count];
+                for (int i = 0; i < maxValues.Length; ++i)
+                {
+                    maxValues[i] = 0;
+                    foreach (double value in yAxis[i].GetDoublesArray())
+                    {
+                        maxValues[i] = Math.Max(maxValues[i], Math.Abs(value));
+                    }
+                }
+
+                for (int i = 0; i < yAxis.Count; ++i)
+                {
+                    fsNamedArray curve = yAxis[i];
+                    string name = curve.Name;
+                    var pointList = new PointPairList();
+                    double scale = Math.Pow(10, Math.Round(Math.Log10(maxValues[0] / maxValues[i])));
+                    for (int pointIndex = 0; pointIndex < curve.Array.Length; ++pointIndex)
+                    {
+                        if (curve.Array[pointIndex].Defined)
+                        {
+                            pointList.Add(m_xAxis.Array[pointIndex].Value, curve.Array[pointIndex].Value * scale);
+                        }
+                    }
+                    string curveName = name;
+                    if (scale != 1.0)
+                    {
+                        curveName += " * " + scale;
+                    }
+                    LineItem zedCurve = fmZedGraphControl1.GraphPane.AddCurve(curveName, pointList, color, SymbolType.None);
+                    zedCurve.IsY2Axis = isY2Axis;
+                    zedCurve.Line.IsAntiAlias = true;
+                }
+            }
+        }
+
+        #region Nested type: fsNamedArray
+
+        public class fsNamedArray
+        {
+            public string Name { get; set; }
+            public fsValue[] Array { get; set; }
+
+            public double[] GetDoublesArray()
+            {
+                var result = new double[Array.Length];
+                for (int i = 0; i < result.Length; ++i)
+                {
+                    result[i] = Array[i].Value;
+                }
+                return result;
+            }
+        }
+
+        #endregion
+    }
+}
