7973924687ddea35574174f769e4731aa90136ee
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-05-22 15:55:49 +0000

    recreate victor/dev

---
+++ victor/dev/fsim/Equations/Material/Porosity Equations/fsPorositySaltNeglectedCakeSaturatedEquation.cs
@@ -0,0 +1,49 @@
+﻿using Parameters;
+
+namespace Equations
+{
+    public class fsPorositySaltNeglectedCakeSaturatedEquation : fsCalculatorEquation
+    {
+        #region Parameters
+
+        private readonly IEquationParameter m_dryMass;
+        private readonly IEquationParameter m_liquidDensity;
+        private readonly IEquationParameter m_porosity;
+        private readonly IEquationParameter m_solidsDensity;
+        private readonly IEquationParameter m_wetMass;
+
+        #endregion
+
+        public fsPorositySaltNeglectedCakeSaturatedEquation(
+            IEquationParameter porosity,
+            IEquationParameter dryMass,
+            IEquationParameter wetMass,
+            IEquationParameter solidsDensity,
+            IEquationParameter liquidDensity)
+            : base(porosity, dryMass, wetMass, solidsDensity, liquidDensity)
+        {
+            m_porosity = porosity;
+            m_dryMass = dryMass;
+            m_wetMass = wetMass;
+            m_solidsDensity = solidsDensity;
+            m_liquidDensity = liquidDensity;
+        }
+
+        protected override void InitFormulas()
+        {
+            AddFormula(m_porosity, PorosityFormula);
+        }
+
+        #region Formulas
+
+        private void PorosityFormula()
+        {
+            m_porosity.Value = 1 -
+                               1 /
+                               (m_solidsDensity.Value / m_liquidDensity.Value * (m_wetMass.Value / m_dryMass.Value - 1) +
+                                1);
+        }
+
+        #endregion
+    }
+}
---
+++ victor/dev/fsim/Equations/Math/GeneralEquations/fsProductsEquation.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using Parameters;
+using Value;
+
+namespace Equations
+{
+    public class fsProductsEquation : fsCalculatorEquation
+    {
+        // This class can calculate any parameter from equation of the next kind:
+        //
+        // L1 * L2 * ... * Ln  ==  R1 * R2 * ... * Rm;
+        // 
+
+        #region Parameters
+
+        private readonly IEnumerable<IEquationParameter> m_leftElements;
+        private readonly IEnumerable<IEquationParameter> m_rightElements;
+
+        #endregion
+
+        public fsProductsEquation(
+            IEnumerable<IEquationParameter> leftElements,
+            IEnumerable<IEquationParameter> rightElements)
+            : base(leftElements, rightElements)
+        {
+            m_leftElements = leftElements;
+            m_rightElements = rightElements;
+        }
+
+        protected override void InitFormulas()
+        {
+            // no specific formulas in this class
+        }
+
+        protected override bool Calculate(IEquationParameter result)
+        {
+            fsValue leftProduct = GetElementsProduct(m_leftElements, result);
+            fsValue rightProduct = GetElementsProduct(m_rightElements, result);
+            int resultDegreeCount = Count(m_leftElements, result) - Count(m_rightElements, result);
+            if (resultDegreeCount == 0)
+            {
+                return false;
+            }
+
+            // Let's make r^d * a = b, where d > 0
+            int d = resultDegreeCount;
+            fsValue a = leftProduct;
+            fsValue b = rightProduct;
+            if (resultDegreeCount < 0)
+            {
+                a = rightProduct;
+                b = leftProduct;
+                d = -resultDegreeCount;
+            }
+            fsValue bOverA = b / a;
+            int sign = 1;
+            if (bOverA.Value < 0 && d % 2 == 1)
+            {
+                bOverA = -bOverA;
+                sign = -1;
+            }
+            result.Value = bOverA == fsValue.Zero
+                ? fsValue.Zero
+                : sign * fsValue.Pow(bOverA, 1.0 / d);
+            return true;
+        }
+
+        private static int Count(IEnumerable<IEquationParameter> parametersList, IEquationParameter parameter)
+        {
+            int result = 0;
+            foreach (IEquationParameter p in parametersList)
+            {
+                result += (p == parameter ? 1 : 0);
+            }
+            return result;
+        }
+
+        private static fsValue GetElementsProduct(
+            IEnumerable<IEquationParameter> elements,
+            IEquationParameter result)
+        {
+            fsValue product = fsValue.One;
+            foreach (IEquationParameter equationParameter in elements)
+            {
+                if (equationParameter != result)
+                {
+                    product = product * equationParameter.Value;
+                }
+            }
+            return product;
+        }
+    }
+}
