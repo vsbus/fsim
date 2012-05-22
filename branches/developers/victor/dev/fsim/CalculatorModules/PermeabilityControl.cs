64615d0bcbcffb52d66054cb474018f5e44e8fcc
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-05-22 15:55:49 +0000

    recreate victor/dev

---
+++ branches/developers/victor/dev/fsim/CalculatorModules/PermeabilityControl.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using CalculatorModules.Base_Controls;
+using Parameters;
+using StepCalculators;
+
+namespace CalculatorModules
+{
+    public sealed partial class fsPermeabilityControl : fsOptionsSingleTableAndCommentsCalculatorControl
+    {
+        #region Calculation Option
+
+        private enum fsCalculationOption
+        {
+            [Description("Pc0, rc0, alpha0")] CalcPc0Rc0Alpha0,
+            [Description("Cake Compressibility nc")] CalcNc,
+            [Description("Pressure (Dp)")] CalcPressure,
+            [Description("Pc, rc, alpha")] CalcPcRcAlpha
+        }
+
+        #endregion
+
+        public fsPermeabilityControl()
+        {
+            InitializeComponent();
+
+            Calculators.Add(new fsPermeabilityCalculator());
+
+            fsParametersGroup solidsGroup = AddGroup(
+                fsParameterIdentifier.SolidsDensity);
+            fsParametersGroup porosityGroup = AddGroup(
+                fsParameterIdentifier.CakePorosity);
+            fsParametersGroup pc0Rc0A0Group = AddGroup(
+                fsParameterIdentifier.CakePermeability0,
+                fsParameterIdentifier.CakeResistance0,
+                fsParameterIdentifier.CakeResistanceAlpha0);
+            fsParametersGroup ncGroup = AddGroup(
+                fsParameterIdentifier.CakeCompressibility);
+            fsParametersGroup pressureGroup = AddGroup(
+                fsParameterIdentifier.PressureDifference);
+            fsParametersGroup pcRcAGroup = AddGroup(
+                fsParameterIdentifier.CakePermeability,
+                fsParameterIdentifier.CakeResistance,
+                fsParameterIdentifier.CakeResistanceAlpha);
+
+            AddGroupToUI(dataGrid, solidsGroup, Color.FromArgb(230, 230, 255));
+            AddGroupToUI(dataGrid, porosityGroup, Color.FromArgb(255, 255, 230));
+            AddGroupToUI(dataGrid, pc0Rc0A0Group, Color.FromArgb(230, 230, 255));
+            AddGroupToUI(dataGrid, ncGroup, Color.FromArgb(255, 255, 230));
+            AddGroupToUI(dataGrid, pressureGroup, Color.FromArgb(230, 230, 255));
+            AddGroupToUI(dataGrid, pcRcAGroup, Color.FromArgb(255, 230, 230));
+
+            fsMisc.FillList(calculationOptionComboBox.Items, typeof (fsCalculationOption));
+            AssignCalculationOptionAndControl(typeof (fsCalculationOption), calculationOptionComboBox);
+            EstablishCalculationOption(fsCalculationOption.CalcPcRcAlpha);
+
+            UpdateGroupsInputInfoFromCalculationOptions();
+            UpdateEquationsFromCalculationOptions();
+            Recalculate();
+            UpdateUIFromData();
+            ConnectUIWithDataUpdating(dataGrid, calculationOptionComboBox);
+        }
+
+        #region Routine Methods
+
+        protected override void UpdateGroupsInputInfoFromCalculationOptions()
+        {
+            var calculationOption = (fsCalculationOption) CalculationOptions[typeof (fsCalculationOption)];
+            fsParametersGroup calculateGroup = null;
+            switch (calculationOption)
+            {
+                case fsCalculationOption.CalcNc:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.CakeCompressibility];
+                    break;
+                case fsCalculationOption.CalcPc0Rc0Alpha0:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.CakePermeability0];
+                    break;
+                case fsCalculationOption.CalcPcRcAlpha:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.CakePermeability];
+                    break;
+                case fsCalculationOption.CalcPressure:
+                    calculateGroup = ParameterToGroup[fsParameterIdentifier.PressureDifference];
+                    break;
+            }
+            foreach (fsParametersGroup group in ParameterToGroup.Values)
+            {
+                SetGroupInput(group, group != calculateGroup);
+            }
+        }
+
+        protected override void UpdateEquationsFromCalculationOptions()
+        {
+            // this control uses only one calculator
+        }
+
+        #endregion
+    }
+}
---
+++ branches/developers/victor/dev/fsim/Equations/Cake Formation/fsSuspensionMassFromHcEpsPlainAreaEquation.cs
@@ -0,0 +1,94 @@
+﻿using Parameters;
+
+namespace Equations
+{
+    public class fsSuspensionMassFromHcEpsPlainAreaEquation : fsCalculatorEquation
+    {
+        #region Parameters
+
+        private readonly IEquationParameter m_cakeHeight;
+        private readonly IEquationParameter m_filterArea;
+        private readonly IEquationParameter m_porosity;
+        private readonly IEquationParameter m_solidsDensity;
+        private readonly IEquationParameter m_solidsMassFraction;
+        private readonly IEquationParameter m_suspensionMass;
+
+        #endregion
+
+        // Msus = (1 - eps) * rho_s * A * hc / Cm;
+
+        public fsSuspensionMassFromHcEpsPlainAreaEquation(
+            IEquationParameter suspensionMass,
+            IEquationParameter porosity,
+            IEquationParameter solidsDensity,
+            IEquationParameter filterArea,
+            IEquationParameter cakeHeight,
+            IEquationParameter solidsMassFraction)
+            : base(
+                suspensionMass,
+                porosity,
+                solidsDensity,
+                filterArea,
+                cakeHeight,
+                solidsMassFraction)
+        {
+            m_suspensionMass = suspensionMass;
+            m_porosity = porosity;
+            m_solidsDensity = solidsDensity;
+            m_filterArea = filterArea;
+            m_cakeHeight = cakeHeight;
+            m_solidsMassFraction = solidsMassFraction;
+        }
+
+        protected override void InitFormulas()
+        {
+            AddFormula(m_suspensionMass, SuspensionMassFormula);
+            AddFormula(m_cakeHeight, CakeHeightFormula);
+            AddFormula(m_filterArea, FilterAreaFormula);
+            AddFormula(m_solidsDensity, SolidsDensityFormula);
+            AddFormula(m_porosity, PorosityFormula);
+            AddFormula(m_solidsMassFraction, SolidsMassFractionFormula);
+        }
+
+        #region Formulas
+
+        private void SuspensionMassFormula()
+        {
+            m_suspensionMass.Value = (1 - m_porosity.Value) * m_solidsDensity.Value * m_filterArea.Value *
+                                     m_cakeHeight.Value / m_solidsMassFraction.Value;
+        }
+
+        private void CakeHeightFormula()
+        {
+            m_cakeHeight.Value = m_suspensionMass.Value * m_solidsMassFraction.Value /
+                                 ((1 - m_porosity.Value) * m_solidsDensity.Value *
+                                  m_filterArea.Value);
+        }
+
+        private void FilterAreaFormula()
+        {
+            m_filterArea.Value = m_suspensionMass.Value * m_solidsMassFraction.Value /
+                                 ((1 - m_porosity.Value) * m_solidsDensity.Value * m_cakeHeight.Value);
+        }
+
+        private void SolidsDensityFormula()
+        {
+            m_solidsDensity.Value = m_suspensionMass.Value * m_solidsMassFraction.Value /
+                                    ((1 - m_porosity.Value) * m_filterArea.Value * m_cakeHeight.Value);
+        }
+
+        private void PorosityFormula()
+        {
+            m_porosity.Value = 1 - m_suspensionMass.Value * m_solidsMassFraction.Value
+                               / (m_solidsDensity.Value * m_filterArea.Value * m_cakeHeight.Value);
+        }
+
+        private void SolidsMassFractionFormula()
+        {
+            m_solidsMassFraction.Value = (1 - m_porosity.Value) * m_solidsDensity.Value *
+                                         m_filterArea.Value * m_cakeHeight.Value / m_suspensionMass.Value;
+        }
+
+        #endregion
+    }
+}
