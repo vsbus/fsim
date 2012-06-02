bccedfdc4614fa0d34c0c2c48560c6da36fc93b0
a6a6a6@yandex.ru <a6a6a6@yandex.ru@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-02 23:27:15 +0000

    added equations for mc, ms, qmft, vsus, vf, vc, vs, qft parameters

--- andrew/dev/fsim/StepCalculators/Small Calculators/fsLaboratoryFiltrationCalculator.cs
+++ andrew/dev/fsim/StepCalculators/Small Calculators/fsLaboratoryFiltrationCalculator.cs
@@ -5,6 +5,8 @@ namespace StepCalculators
 {
     public class fsLaboratoryFiltrationCalculator : fsCalculator
     {
+        #region Parameters Declaration
+
         readonly fsCalculatorVariable m_hce0;
         readonly fsCalculatorConstant m_viscosity;
         readonly fsCalculatorConstant m_pc0;
@@ -28,12 +30,25 @@ namespace StepCalculators
         readonly fsCalculatorConstant m_rhosus;
         readonly fsCalculatorConstant m_kappa;
         readonly fsCalculatorConstant m_rho;
+        readonly fsCalculatorVariable m_rhos;
+        readonly fsCalculatorConstant m_eps;
+        readonly fsCalculatorVariable m_qmft;
+        readonly fsCalculatorVariable m_Dp;
+        readonly fsCalculatorVariable m_vsus;
+        readonly fsCalculatorVariable m_vf;
+        readonly fsCalculatorVariable m_vc;
+        readonly fsCalculatorVariable m_vs;
+        readonly fsCalculatorVariable m_qft;
+
+        #endregion
 
         public fsLaboratoryFiltrationCalculator()
         {
             #region Parameters Initialization
 
+            m_rhos = AddVariable(fsParameterIdentifier.SolidsDensity);
             m_rho = AddConstant(fsParameterIdentifier.MotherLiquidDensity);
+            m_eps = AddConstant(fsParameterIdentifier.CakePorosity);
             m_hce0 = AddVariable(fsParameterIdentifier.FilterMediumResistanceHce0);
             m_viscosity = AddConstant(fsParameterIdentifier.MotherLiquidViscosity);
             m_pc0 = AddConstant(fsParameterIdentifier.CakePermeability0);
@@ -56,6 +71,13 @@ namespace StepCalculators
             m_mf = AddVariable(fsParameterIdentifier.FiltrateMass);
             m_mc = AddVariable(fsParameterIdentifier.CakeMass);
             m_ms = AddVariable(fsParameterIdentifier.SolidsMass);
+            m_qmft = AddVariable(fsParameterIdentifier.qmft);
+            m_Dp = AddVariable(fsParameterIdentifier.PressureDifference);
+            m_vsus = AddVariable(fsParameterIdentifier.SuspensionVolume);
+            m_vf = AddVariable(fsParameterIdentifier.FiltrateVolume);
+            m_vc = AddVariable(fsParameterIdentifier.CakeVolume);
+            m_vs = AddVariable(fsParameterIdentifier.SolidsVolume);
+            m_qft = AddVariable(fsParameterIdentifier.qft);
 
             #endregion
 
@@ -69,6 +91,14 @@ namespace StepCalculators
             AddEquation(new fsRm0FromHcePc0(m_Rm0, m_hce0, m_pc0));
             AddEquation(new fsMsusFromHcRhosusAKappa(m_suspensionMass, m_rhosus, m_area, m_hc, m_kappa));
             AddEquation(new fsMfFromHc(m_mf, m_rho, m_area, m_hc, m_kappa));
+            AddEquation(new fsMcFromHc(m_mc, m_area, m_hc, m_rhos, m_eps, m_rho));
+            AddEquation(new fsMsFromHc(m_ms, m_area, m_hc, m_rhos, m_eps));
+            AddEquation(new fsQmftFromHcRhoEtaDp(m_qmft, m_rho, m_pc, m_Dp, m_viscosity, m_hc, m_hce0));
+            AddEquation(new fsVsusFromHcAKappa(m_vsus, m_area, m_hc, m_kappa));
+            AddEquation(new fsVfFromHc(m_vf, m_area, m_hc, m_kappa));
+            AddEquation(new fsVcFromHc(m_vc, m_area , m_hc));
+            AddEquation(new fsVsFromHc(m_vs, m_area, m_hc, m_eps));
+            AddEquation(new fsQftFromHcEtaDp(m_qft, m_pc, m_Dp, m_viscosity, m_hc, m_hce0));
 
             #endregion
         }
