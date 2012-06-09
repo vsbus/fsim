8d528779d2980aa2f692cb628bb2abd2372920ec
a6a6a6@yandex.ru <a6a6a6@yandex.ru@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-09 21:41:44 +0000

    New Equation for Hc added K equation corrected

---
+++ andrew/dev/fsim/Equations/Cake Formation/fsHcFromTfAndKEquation.cs
@@ -0,0 +1,52 @@
+﻿using Parameters;
+using Value;
+
+namespace Equations
+{
+    public class fsHcFromTfAndKEquation : fsCalculatorEquation
+    {
+        #region Parameters
+
+        private readonly IEquationParameter hc;
+        private readonly IEquationParameter kappa;
+        private readonly IEquationParameter Dp;
+        private readonly IEquationParameter tf;
+        private readonly IEquationParameter K;
+        
+        #endregion
+
+        public fsHcFromTfAndKEquation(
+            IEquationParameter CakeHeight,
+            IEquationParameter Kappa,
+            IEquationParameter PressureDifference,
+            IEquationParameter FiltrtionTime,
+            IEquationParameter PracticalCakePermeability)
+            : base(
+                CakeHeight,
+                Kappa,
+                PressureDifference,
+                FiltrtionTime,
+                PracticalCakePermeability)
+        {
+            hc = CakeHeight;
+            kappa = Kappa;
+            Dp = PressureDifference;
+            tf = FiltrtionTime;
+            K = PracticalCakePermeability;
+        }
+
+        protected override void InitFormulas()
+        {
+            AddFormula(hc, CakeHeightFormula);            
+        }
+
+        #region Formulas
+
+        private void CakeHeightFormula()
+        {
+            hc.Value = fsValue.Sqrt(2 * kappa.Value * Dp.Value * tf.Value * K.Value);
+        }
+
+        #endregion
+    }
+}
