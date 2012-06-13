0716f75ae8d6ee8051eb0948815ff8c0a2dc454c
dtyshk@gmail.com <dtyshk@gmail.com@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-13 06:23:51 +0000

    New files are added as recommended by Victor 12.08.2012

---
+++ dmitry/dev/fsim/StepCalculators/Small Calculators/fsCakeWashingCalculator.cs
@@ -0,0 +1,13 @@
+﻿using Equations;
+using Parameters;
+using Value;
+
+namespace StepCalculators.Simulation_Calculators
+{
+    public class fsCakeWashingCalculator : fsCalculator
+    {
+        public fsCakeWashingCalculator()
+        {
+        }
+    }
+}
