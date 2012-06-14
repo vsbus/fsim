6f0d7c2e49b6b4508bc405c52ec598e7fe75e8e9
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-14 11:30:57 +0000

    [dmitry]:
    r524
    Controller fsCakeWashingControl is modified accordingly to the convention.
    ---------------------
    r523
    fsCakeWashingCalculator.cs is added (with appropriately modified parameters accordingly to convention)
    ---------------------
    r522
    Comments for the equations in Cake Washing folder are added and class Equation is renamed where it needed to be done.
    ---------------------
    r513
    Sorry. In the previous commit the folder Cake Washing hadn't be added. Now everything is ok!
    ---------------------
    r512
    The function Erfc is added to the file fsValue.cs. The folder Cake Washing is created and the set of main Cake Washing Equations are settled in this folder.
    ---------------------
    r508
    File fsParameterIdentifier.cs is updated. New parameters are added. 
    ---------------------
    r507
    File fsCharacteristic.cs is updated. New characteristics are added.
    ---------------------
    r506
    The file fsUnits.cs is modified (the new units are added)
    ---------------------

--- dev/fsim/CalculatorModules/CakeWashing/CakeWashingControl.cs
+++ dev/fsim/CalculatorModules/CakeWashing/CakeWashingControl.cs
@@ -3,23 +3,51 @@ using CalculatorModules.Base_Controls;
 using Parameters;
 using StepCalculators;
 using StepCalculators.Simulation_Calculators;
+using Value;
 
 
 namespace CalculatorModules.CakeWashing
 {
     public partial class fsCakeWashingControl : fsOptionsDoubleTableAndCommentsCalculatorControl
     {
+        private void AssignDefaultValues()
+        {
+            Values[fsParameterIdentifier.MotherLiquidViscosity].Value = new fsValue(1e-3);
+            Values[fsParameterIdentifier.MotherLiquidDensity].Value = new fsValue(1000);
+            Values[fsParameterIdentifier.SolidsDensity].Value = new fsValue(2500);
+            Values[fsParameterIdentifier.CakePorosity0].Value = new fsValue(55e-2);
+            Values[fsParameterIdentifier.Ne].Value = new fsValue(0.02);
+            Values[fsParameterIdentifier.CakePermeability0].Value = new fsValue(1.5e-13);
+            Values[fsParameterIdentifier.CakeCompressibility].Value = new fsValue(0.3);
+            Values[fsParameterIdentifier.FilterMediumResistanceHce].Value = new fsValue(3e-3);
+            Values[fsParameterIdentifier.CakeSaturationSw0].Value = new fsValue(70e-2);
+            Values[fsParameterIdentifier.PredeliquorFlowRate].Value = new fsValue(0.5);
+            Values[fsParameterIdentifier.CakeWashOutConcentration].Value = new fsValue(50);
+            Values[fsParameterIdentifier.RemanentWashOutContent].Value = new fsValue(0.1e-2);
+            Values[fsParameterIdentifier.WashLiquidDensity].Value = new fsValue(900);
+            Values[fsParameterIdentifier.WashLiquidViscosity].Value = new fsValue(0.95e-3);
+            Values[fsParameterIdentifier.LiquidWashOutConcentration].Value = new fsValue(0.5);
+            Values[fsParameterIdentifier.WashIndexFor0].Value = new fsValue(2);
+            Values[fsParameterIdentifier.AdaptationPar1].Value = new fsValue(0.11);
+            Values[fsParameterIdentifier.AdaptationPar2].Value = new fsValue(0.09);
+
+            Values[fsParameterIdentifier.FilterArea].Value = new fsValue(7);
+            Values[fsParameterIdentifier.ns].Value = new fsValue(10);
+            Values[fsParameterIdentifier.ls].Value = new fsValue(0.7);
+            Values[fsParameterIdentifier.StandardTechnicalTime].Value = new fsValue(2);
+            Values[fsParameterIdentifier.lambda].Value = new fsValue(0.2);
+            Values[fsParameterIdentifier.u].Value = new fsValue(3.0 / 60);
+            Values[fsParameterIdentifier.CakeHeight].Value = new fsValue(20e-3);
+            Values[fsParameterIdentifier.PressureDifference].Value = new fsValue(0.6e5);          
+        }
+
         public fsCakeWashingControl()
         {
             InitializeComponent();
 
             #region Calculators
 
-            Calculators.Add(new fsDensityConcentrationCalculator());
-            Calculators.Add(new fsPorosityCalculator());
-            Calculators.Add(new fsPermeabilityCalculator());
-            Calculators.Add(new fsRm0Hce0Calculator());
-            Calculators.Add(new fsBeltFiltersWithReversibleTraysCalculator());
+            Calculators.Add(new fsCakeWashingCalculator());
 
             #endregion
 
@@ -38,54 +66,89 @@ namespace CalculatorModules.CakeWashing
                 fsParameterIdentifier.MotherLiquidDensity);
 
             fsParametersGroup densitiesGroup = AddGroup(
-                fsParameterIdentifier.SolidsDensity,
-                fsParameterIdentifier.SuspensionDensity);
-
-            fsParametersGroup cGroup = AddGroup(
-                fsParameterIdentifier.SuspensionSolidsMassFraction,
-                fsParameterIdentifier.SuspensionSolidsVolumeFraction,
-                fsParameterIdentifier.SuspensionSolidsConcentration);
-
-            fsParametersGroup neGroup = AddGroup(
-                fsParameterIdentifier.Ne);
+                fsParameterIdentifier.SolidsDensity);
 
             fsParametersGroup epsGroup = AddGroup(
                 fsParameterIdentifier.CakePorosity0,
-                fsParameterIdentifier.Kappa0,
                 fsParameterIdentifier.DryCakeDensity0,
-                fsParameterIdentifier.CakeWetDensity0,
-                fsParameterIdentifier.CakeWetMassSolidsFractionRs0,
                 fsParameterIdentifier.CakeMoistureContentRf0,
                 fsParameterIdentifier.CakePorosity,
-                fsParameterIdentifier.Kappa,
                 fsParameterIdentifier.DryCakeDensity,
-                fsParameterIdentifier.CakeWetDensity,
-                fsParameterIdentifier.CakeWetMassSolidsFractionRs,
                 fsParameterIdentifier.CakeMoistureContentRf);
+
+            fsParametersGroup neGroup = AddGroup(
+                fsParameterIdentifier.Ne);       
 
             fsParametersGroup pcrcGroup = AddGroup(
                 fsParameterIdentifier.CakePermeability0,
                 fsParameterIdentifier.CakeResistance0,
-                fsParameterIdentifier.CakeResistanceAlpha0);
+                fsParameterIdentifier.CakeResistanceAlpha0,
+                fsParameterIdentifier.CakePermeability,
+                fsParameterIdentifier.CakeResistance,
+                fsParameterIdentifier.CakeResistanceAlpha,
+                fsParameterIdentifier.PracticalCakePermeability);
 
             fsParametersGroup ncGroup = AddGroup(
                 fsParameterIdentifier.CakeCompressibility);
 
-            fsParametersGroup hce0Group = AddGroup(
-                fsParameterIdentifier.FilterMediumResistanceHce0,
-                fsParameterIdentifier.FilterMediumResistanceRm0);
+            fsParametersGroup hceGroup = AddGroup(
+                fsParameterIdentifier.FilterMediumResistanceHce,
+                fsParameterIdentifier.FilterMediumResistanceRm);
+
+            fsParametersGroup cstartGroup = AddGroup(
+                fsParameterIdentifier.CakeSaturationSw0,
+                fsParameterIdentifier.CakeMoistureContentRfw0);
+
+            fsParametersGroup fqGroup = AddGroup(
+                fsParameterIdentifier.PredeliquorFlowRate);
+
+            fsParametersGroup washoutGroup = AddGroup(
+                fsParameterIdentifier.CakeWashOutConcentration,
+                fsParameterIdentifier.CakeWashOutContentX0p);
+
+            fsParametersGroup xrGroup = AddGroup(
+                fsParameterIdentifier.RemanentWashOutContent);
+
+            fsParametersGroup rhowGroup = AddGroup(
+                fsParameterIdentifier.WashLiquidDensity);
+
+            fsParametersGroup etawGroup = AddGroup(
+                fsParameterIdentifier.WashLiquidViscosity);
+
+            fsParametersGroup cwGroup = AddGroup(
+                fsParameterIdentifier.LiquidWashOutConcentration);
+
+            fsParametersGroup dnGroup = AddGroup(
+                fsParameterIdentifier.WashIndexFor0,
+                fsParameterIdentifier.WashIndexFor);
+
+            fsParametersGroup aw1Group = AddGroup(
+                fsParameterIdentifier.AdaptationPar1);
+
+            fsParametersGroup aw2Group = AddGroup(
+                fsParameterIdentifier.AdaptationPar2);
+
 
             var materialGroups = new[]
                              {
                                 etafGroup,
                                 rhofGroup,
                                 densitiesGroup,
-                                cGroup,
-                                neGroup,
                                 epsGroup,
+                                neGroup,
                                 pcrcGroup,
                                 ncGroup,
-                                hce0Group, 
+                                hceGroup,
+                                cstartGroup,
+                                fqGroup,
+                                washoutGroup,
+                                xrGroup,
+                                rhowGroup,
+                                etawGroup,
+                                cwGroup,
+                                dnGroup,
+                                aw1Group,
+                                aw2Group
                              };
 
             for (int i = 0; i < materialGroups.Length; ++i)
@@ -97,7 +160,108 @@ namespace CalculatorModules.CakeWashing
 
             #endregion
 
-            //SetDefaultDiagram(fsParameterIdentifier.u, fsParameterIdentifier.FilterArea, fsParameterIdentifier.SpecificFiltrationTime);
+            #region Machine groups 
+           
+            fsParametersGroup abGroup = AddGroup(
+              fsParameterIdentifier.FilterArea,
+              fsParameterIdentifier.MachineWidth);
+
+            fsParametersGroup nsGroup = AddGroup(
+                fsParameterIdentifier.ns);
+
+            fsParametersGroup geometryGroup = AddGroup(
+                fsParameterIdentifier.ls,
+                fsParameterIdentifier.ls_over_b,
+                fsParameterIdentifier.FilterLength,
+                fsParameterIdentifier.l_over_b,
+                fsParameterIdentifier.As);
+
+            fsParametersGroup timeGroup = AddGroup(
+                fsParameterIdentifier.StandardTechnicalTime,
+                fsParameterIdentifier.TechnicalTime);
+
+            fsParametersGroup lambdaGroup = AddGroup(
+                fsParameterIdentifier.lambda);
+
+            fsParametersGroup uGroup = AddGroup(
+                fsParameterIdentifier.u,
+                fsParameterIdentifier.RotationalSpeed ,
+                fsParameterIdentifier.CycleTime);
+
+            fsParametersGroup hcGroup = AddGroup(
+                fsParameterIdentifier.CakeHeight,
+                fsParameterIdentifier.SolidsMass,
+                fsParameterIdentifier.Qms);
+
+            fsParametersGroup dpGroup = AddGroup(
+                fsParameterIdentifier.PressureDifference);
+
+            fsParametersGroup washGroup = AddGroup(
+                fsParameterIdentifier.WashingRatioW,
+                fsParameterIdentifier.WashingRatioWv,
+                fsParameterIdentifier.WashingRatioWm,
+                fsParameterIdentifier.WashLiquidVolume,
+                fsParameterIdentifier.WashLiquidMass,
+                fsParameterIdentifier.NumberOfWashingSegments,
+                fsParameterIdentifier.SpecificWashArea,
+                fsParameterIdentifier.WashTime,
+                fsParameterIdentifier.WashLiquidVolFlowRate,
+                fsParameterIdentifier.WashLiquidMassFlowRate,
+                fsParameterIdentifier.SpecificWashOutConcentration,
+                fsParameterIdentifier.SpecificAverageWashOut,
+                fsParameterIdentifier.SpecificWashOutConcentrationInCake,
+                fsParameterIdentifier.WashOutConcentrationInWashfiltrate,
+                fsParameterIdentifier.AverageWashOutConcentration,
+                fsParameterIdentifier.WashOutConcentrationInCake,
+                fsParameterIdentifier.SpecificWashOutXStar,
+                fsParameterIdentifier.SpecificWashOutX,
+                fsParameterIdentifier.CakeWashOutContent);
+
+            fsParametersGroup washDatagroup = AddOnlyCalculatedGroup(
+                fsParameterIdentifier.SpecificWashfiltrate,
+                fsParameterIdentifier.VolumeOfWashfiltrate,
+                fsParameterIdentifier.MassOfWashfiltrate,
+                fsParameterIdentifier.CakeVolume,
+                fsParameterIdentifier.CakeMass,
+                fsParameterIdentifier.LiquidVolumeInCake,
+                fsParameterIdentifier.LiquidMassInCake,
+                fsParameterIdentifier.CakeMoistureContentRfw,
+                fsParameterIdentifier.FeedVolumeFlowRate ,
+                fsParameterIdentifier.FeedSolidsMassFlowRate ,
+                fsParameterIdentifier.AverageVolumeFlowRate,
+                fsParameterIdentifier.AverageMassFlowRate,
+                fsParameterIdentifier.SpecificVolumeFlowRate,
+                fsParameterIdentifier.SpecificMassFlowRate,
+                fsParameterIdentifier.SpecificAverageVolumeFlowRate,
+                fsParameterIdentifier.SpecificAverageMassFlowRate
+                );
+
+            var machineGroups = new[]
+                            {
+                               abGroup,
+                               nsGroup,
+                               geometryGroup,
+                               timeGroup,
+                               lambdaGroup,
+                               uGroup,
+                               hcGroup,
+                               dpGroup,
+                               washGroup,
+                               washDatagroup
+                            };
+
+            for (int i = 0; i < machineGroups.Length; ++i)
+            {
+                machineGroups[i].Kind = fsParametersGroup.fsParametersGroupKind.MachiningSettingsParameters;
+                AddGroupToUI(dataGrid, machineGroups[i], colors[i % colors.Length]);
+                SetGroupInput(machineGroups[i], true);
+            }
+
+            #endregion
+
+            AssignDefaultValues();
+            
+            //SetDefaultDiagram(fsParameterIdentifier.WashTime, fsParameterIdentifier.WetCakeMass, fsParameterIdentifier.WashLiquidMassFlowRate);
             Recalculate();
             UpdateUIFromData();
             ConnectUIWithDataUpdating(materialParametersDataGrid, dataGrid);
