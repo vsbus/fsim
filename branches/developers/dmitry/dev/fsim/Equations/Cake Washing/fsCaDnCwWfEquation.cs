7f65077d8f5639e93552f10439a31212dbb68773
dtyshk@gmail.com <dtyshk@gmail.com@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-14 19:27:30 +0000

    The equation fsCaDnCwWfEquation is refined. c*, x*, ca -groups equations refining is up.

--- branches/developers/dmitry/dev/fsim/Equations/Cake Washing/fsCaDnCwWfEquation.cs
+++ branches/developers/dmitry/dev/fsim/Equations/Cake Washing/fsCaDnCwWfEquation.cs
@@ -129,7 +129,8 @@ namespace Equations.CakeWashing
             bool condEmpty = fsValue.Less(m_Dn.Value, fsValue.Zero) ||
                              fsValue.Less(u, fsValue.Zero) ||
                              u == fsValue.Zero ||
-                             fsValue.Less(fsValue.Exp(m_Dn.Value) * fsValue.Erfc(fsValue.Sqrt(m_Dn.Value)), u);
+                             fsValue.Less(fsValue.One, u) ||
+                             u == fsValue.One;
             if (condEmpty)
             {
                 m_wf.Value = new fsValue();
@@ -137,10 +138,22 @@ namespace Equations.CakeWashing
             else
             {
                 var f = new wfCalculationFunction(m_Dn.Value, u);
-                fsValue sqrt = 1 / fsValue.Sqrt(m_Dn.Value);
-                fsValue b = sqrt * (1 + sqrt) / Math.Sqrt(Math.PI);
-                fsValue upperBound = 1 + fsValue.Sqrt(fsValue.Log(b / u));
-                fsValue x = fsBisectionMethod.FindRoot(f, fsValue.Zero, upperBound, 60);
+                fsValue lowerBound;
+                fsValue upperBound;
+                if (fsValue.Less(u, fsValue.Exp(m_Dn.Value) * fsValue.Erfc(fsValue.Sqrt(m_Dn.Value))))
+                {
+                    lowerBound = fsValue.Zero;
+                    fsValue sqrt = 1 / fsValue.Sqrt(m_Dn.Value);
+                    fsValue b = sqrt * (1 + sqrt) / Math.Sqrt(Math.PI);
+                    upperBound = 1 + fsValue.Sqrt(fsValue.Log(b / u));
+                }
+                else
+                {
+                    lowerBound = -1.0 * fsValue.Sqrt(m_Dn.Value / (fsValue.Sqr(2 / u - 1) - 1));
+                    upperBound = fsValue.Zero;
+                }
+
+                fsValue x = fsBisectionMethod.FindRoot(f, lowerBound, upperBound, 60);
                 x = 2 * x / fsValue.Sqrt(m_Dn.Value);
                 m_wf.Value = 1 + 0.5 * x * (x - fsValue.Sqrt(fsValue.Sqr(x) + 4));
             }
