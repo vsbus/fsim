0ac21fabf6ee094fe64f3831a514b266a7adcce7
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-14 12:03:07 +0000

    recreate victor/dev

---
+++ victor/dev/fsim/CalculatorModules/Cake Fromation/Other Filters Controls/LaboratoryVacuumFilterControl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Parameters;
+using Value;
+
+namespace CalculatorModules.Cake_Fromation.Other_Filters_Controls
+{
+    public partial class fsLaboratoryVacuumFilterControl : fsCommonCakeFormationControl
+    {
+        public fsLaboratoryVacuumFilterControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void InitializeParametersValues()
+        {
+            base.InitializeParametersValues();
+
+            SetDefaultValue(fsParameterIdentifier.Ne, new fsValue(0.05));
+            SetDefaultValue(fsParameterIdentifier.CakePermeability0, new fsValue(1.5e-13));
+            SetDefaultValue(fsParameterIdentifier.CakeCompressibility, new fsValue(0.2));
+            SetDefaultValue(fsParameterIdentifier.FilterArea, new fsValue(100e-4));
+            SetDefaultValue(fsParameterIdentifier.PressureDifference, new fsValue(0.6e5));
+            SetDefaultValue(fsParameterIdentifier.ResidualTime, new fsValue(120));
+            SetDefaultValue(fsParameterIdentifier.FiltrationTime, new fsValue(60));
+        }
+
+        protected override void InitializeDefaultDiagrams()
+        {
+            m_defaultDiagrams.Add(
+                new Enum[] { fsCakeFormationCalculationOption.StandardCalculation },
+                new DiagramConfiguration(
+                    fsParameterIdentifier.FiltrationTime,
+                    new DiagramConfiguration.DiagramRange(5, 300),
+                    new[] { fsParameterIdentifier.Qms },
+                    new[] { fsParameterIdentifier.CakeHeight }));
+
+            m_defaultDiagrams.Add(
+                new Enum[] { fsCakeFormationCalculationOption.FilterDesign },
+                new DiagramConfiguration(
+                    fsParameterIdentifier.CakeHeight,
+                    new DiagramConfiguration.DiagramRange(0.003, 0.050),
+                    new[] { fsParameterIdentifier.FilterArea },
+                    new[] { fsParameterIdentifier.FiltrationTime }));
+        }
+    }
+}
---
+++ victor/dev/fsim/CalculatorModules/User Controls/Help Dialogs/fsTablesAndChartsParametersSelectionDialog.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace CalculatorModules.User_Controls.Help_Dialogs
+{
+    partial class fsTablesAndChartsParametersSelectionDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.y1SelectionControl = new CalculatorModules.User_Controls.Help_Dialogs.fsTablesAndChartsParametersSelectionControl();
+            this.y2SelectionControl = new CalculatorModules.User_Controls.Help_Dialogs.fsTablesAndChartsParametersSelectionControl();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(789, 5);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.CancelButtonClick);
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.Location = new System.Drawing.Point(708, 5);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.OkButtonClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cancelButton);
+            this.panel1.Controls.Add(this.okButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 427);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(867, 31);
+            this.panel1.TabIndex = 4;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 41);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.y1SelectionControl);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.y2SelectionControl);
+            this.splitContainer1.Size = new System.Drawing.Size(867, 386);
+            this.splitContainer1.SplitterDistance = 430;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // y1SelectionControl
+            // 
+            this.y1SelectionControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.y1SelectionControl.Location = new System.Drawing.Point(0, 0);
+            this.y1SelectionControl.Name = "y1SelectionControl";
+            this.y1SelectionControl.Size = new System.Drawing.Size(430, 386);
+            this.y1SelectionControl.TabIndex = 3;
+            this.y1SelectionControl.Text = "Y Axis";
+            // 
+            // y2SelectionControl
+            // 
+            this.y2SelectionControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.y2SelectionControl.Location = new System.Drawing.Point(0, 0);
+            this.y2SelectionControl.Name = "y2SelectionControl";
+            this.y2SelectionControl.Size = new System.Drawing.Size(433, 386);
+            this.y2SelectionControl.TabIndex = 0;
+            this.y2SelectionControl.Text = "Y2 Axis";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(867, 41);
+            this.panel2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Location = new System.Drawing.Point(780, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Y1 / Y2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fsTablesAndChartsParametersSelectionDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 458);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "fsTablesAndChartsParametersSelectionDialog";
+            this.Text = "fsTablesAndChartsParametersSelectionDialog";
+            this.Load += new System.EventHandler(this.fsTablesAndChartsParametersSelectionDialog_Load);
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
+        private fsTablesAndChartsParametersSelectionControl y1SelectionControl;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private fsTablesAndChartsParametersSelectionControl y2SelectionControl;
+    }
+}
