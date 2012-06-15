d35c12ed8e98e60e843c6c5ee7645d2a1e752a3f
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-15 10:39:41 +0000

    recreate andrew/dev

---
+++ branches/developers/andrew/dev/fsim/CalculatorModules/Base Controls/OptionsSingleTableAndCommentsCalculatorControl.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculatorModules.Base_Controls
+{
+    public partial class fsOptionsSingleTableAndCommentsCalculatorControl : fsOptionsAndCommentsCalculatorControl
+    {
+        public fsOptionsSingleTableAndCommentsCalculatorControl()
+        {
+            InitializeComponent();
+        }
+
+        protected internal override void StopGridsEdit()
+        {
+            dataGrid.EndEdit();
+        }
+    }
+}
---
+++ branches/developers/andrew/dev/fsim/CalculatorModules/Cake Fromation/Other Filters Controls/RotaryPressureFilters.cs
@@ -0,0 +1,28 @@
+﻿using Parameters;
+using Value;
+
+namespace CalculatorModules.Cake_Fromation.Other_Filters_Controls
+{
+    public partial class fsRotaryPressureFilters : fsCommonCakeFormationControl
+    {
+        public fsRotaryPressureFilters()
+        {
+            InitializeComponent();
+        }
+
+        protected override void InitializeParametersValues()
+        {
+            base.InitializeParametersValues();
+
+            SetDefaultValue(fsParameterIdentifier.Ne, new fsValue(0.05));
+            SetDefaultValue(fsParameterIdentifier.CakePorosity0, new fsValue(0.6));
+            SetDefaultValue(fsParameterIdentifier.CakePermeability0, new fsValue(0.3e-13));
+            SetDefaultValue(fsParameterIdentifier.CakeCompressibility, new fsValue(0.5));
+            SetDefaultValue(fsParameterIdentifier.FilterMediumResistanceHce0, new fsValue(0.001));
+            SetDefaultValue(fsParameterIdentifier.FilterArea, new fsValue(2.0));
+            SetDefaultValue(fsParameterIdentifier.PressureDifference, new fsValue(4e5));
+            SetDefaultValue(fsParameterIdentifier.SpecificFiltrationTime, new fsValue(0.30));
+            SetDefaultValue(fsParameterIdentifier.RotationalSpeed, new fsValue(0.5 / 60.0));
+        }
+    }
+}
