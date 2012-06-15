6a9c261aff72f85bf596e757a6ace1a463d2b34b
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-15 10:39:41 +0000

    recreate andrew/dev

---
+++ andrew/dev/fsim/CalculatorModules/Base Controls/OptionsDoubleTableAndCommentsCalculatorControl.cs
@@ -0,0 +1,16 @@
+﻿namespace CalculatorModules.Base_Controls
+{
+    public partial class fsOptionsDoubleTableAndCommentsCalculatorControl : fsOptionsAndCommentsCalculatorControl
+    {
+        public fsOptionsDoubleTableAndCommentsCalculatorControl()
+        {
+            InitializeComponent();
+        }
+
+        protected internal override void StopGridsEdit()
+        {
+            dataGrid.EndEdit();
+            materialParametersDataGrid.EndEdit();
+        }
+    }
+}
