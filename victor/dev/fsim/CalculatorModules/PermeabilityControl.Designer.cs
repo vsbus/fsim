3bffaa06d553a368cff3cb0855b1cd90fdc76f30
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-17 21:06:18 +0000

    recreate victor/dev

---
+++ victor/dev/fsim/CalculatorModules/PermeabilityControl.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CalculatorModules
+{
+    sealed partial class fsPermeabilityControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculationOptionComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.leftTopPanel.SuspendLayout();
+            this.calculationOptionsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // leftTopPanel
+            // 
+            this.leftTopPanel.Size = new System.Drawing.Size(277, 37);
+            // 
+            // calculationOptionsPanel
+            // 
+            this.calculationOptionsPanel.Controls.Add(this.calculationOptionComboBox);
+            this.calculationOptionsPanel.Controls.Add(this.label1);
+            this.calculationOptionsPanel.Size = new System.Drawing.Size(226, 37);
+            // 
+            // calculationOptionComboBox
+            // 
+            this.calculationOptionComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.calculationOptionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.calculationOptionComboBox.FormattingEnabled = true;
+            this.calculationOptionComboBox.Location = new System.Drawing.Point(40, 8);
+            this.calculationOptionComboBox.Name = "calculationOptionComboBox";
+            this.calculationOptionComboBox.Size = new System.Drawing.Size(180, 21);
+            this.calculationOptionComboBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-20, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculate:";
+            // 
+            // fsPermeabilityControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "fsPermeabilityControl";
+            this.Size = new System.Drawing.Size(277, 272);
+            this.leftTopPanel.ResumeLayout(false);
+            this.calculationOptionsPanel.ResumeLayout(false);
+            this.calculationOptionsPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox calculationOptionComboBox;
+    }
+}
