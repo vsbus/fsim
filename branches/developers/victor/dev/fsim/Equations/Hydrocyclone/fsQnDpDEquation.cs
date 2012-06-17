71cc517450a6c0dce8c800467de836a1c6d79ed4
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-17 21:06:18 +0000

    recreate victor/dev

---
+++ branches/developers/victor/dev/fsim/Equations/Hydrocyclone/fsQnDpDEquation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Parameters;
+using Value;
+
+namespace Equations.Hydrocyclone
+{
+    public class fsQnDpDEquation : fsCalculatorEquation
+    {
+        #region Parameters
+
+        private readonly IEquationParameter m_Q;
+        private readonly IEquationParameter m_Dp;
+        private readonly IEquationParameter m_n;
+        private readonly IEquationParameter m_D;
+        private readonly IEquationParameter m_rhoF;
+        private readonly IEquationParameter m_etaF;
+        private readonly IEquationParameter m_cv;
+        private readonly IEquationParameter m_beta1;
+        private readonly IEquationParameter m_beta2;
+        private readonly IEquationParameter m_beta3;
+
+        #endregion
+
+        public fsQnDpDEquation(
+            IEquationParameter Q,
+            IEquationParameter Dp,
+            IEquationParameter numberOfCyclones,
+            IEquationParameter D,
+            IEquationParameter rhoF,
+            IEquationParameter etaF,
+            IEquationParameter cv,
+            IEquationParameter beta1,
+            IEquationParameter beta2,
+            IEquationParameter beta3)
+            : base(Q,Dp,numberOfCyclones,D,rhoF,etaF,cv,beta1,beta2,beta3)
+        {
+            m_Q = Q;
+            m_Dp = Dp;
+            m_n = numberOfCyclones;
+            m_D = D;
+            m_rhoF = rhoF;
+            m_etaF = etaF;
+            m_cv = cv;
+            m_beta1 = beta1;
+            m_beta2 = beta2;
+            m_beta3 = beta3;
+        }
+
+        protected override void InitFormulas()
+        {
+            AddFormula(m_Q, QFormula);
+        }
+
+        #region Formulas
+
+        private void QFormula()
+        {
+            fsValue Num = m_Dp.Value * fsValue.Sqr(Math.PI * m_n.Value * m_D.Value * m_D.Value);
+            fsValue Den = 8 * m_rhoF.Value * m_beta1.Value * fsValue.Pow(4 * m_rhoF.Value / (Math.PI * m_etaF.Value * m_n.Value * m_D.Value), m_beta2.Value) * fsValue.Exp(-m_beta3.Value * m_cv.Value);
+            m_Q.Value = fsValue.Pow(Num / Den, 1 / (2 + m_beta2.Value));
+        }
+
+        #endregion
+    }
+}
---
+++ branches/developers/victor/dev/fsim/StepCalculators/Small Calculators/fsCakeWashOutContentCalculator.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Equations;
+using Parameters;
+using Value;
+
+namespace StepCalculators
+{
+    public class fsCakeWashOutContentCalculator : fsCalculator
+    {
+        readonly fsCalculatorConstant m_wetMass;
+        readonly fsCalculatorConstant m_dryMass;
+        readonly fsCalculatorConstant m_liquidMassForResuspension;
+        readonly fsCalculatorConstant m_solidsMassFraction;
+        readonly fsCalculatorConstant m_solidsConcentration;
+        readonly fsCalculatorVariable m_internalC;
+        readonly fsCalculatorConstant m_liquidDensity;
+        readonly fsCalculatorVariable m_cakeMoistureContent;
+        readonly fsCalculatorConstant m_pH;
+        readonly fsCalculatorVariable m_pHcake;
+        readonly fsCalculatorVariable m_cakeWashOutContent;
+        
+        public fsCakeWashOutContentCalculator()
+        {
+            #region Parameters Initialization
+
+            m_wetMass = AddConstant(fsParameterIdentifier.WetCakeMass);
+            m_dryMass = AddConstant(fsParameterIdentifier.DryCakeMass);
+            m_liquidMassForResuspension = AddConstant(fsParameterIdentifier.LiquidMassForResuspension);
+            m_solidsMassFraction = AddConstant(fsParameterIdentifier.LiquidWashOutMassFraction);
+            m_solidsConcentration = AddConstant(fsParameterIdentifier.LiquidWashOutConcentration);
+            m_internalC = AddVariable(new fsParameterIdentifier("internalC"));
+            m_liquidDensity = AddConstant(fsParameterIdentifier.LiquidDensity);
+            m_cakeMoistureContent = AddVariable(fsParameterIdentifier.CakeMoistureContentRf);
+            m_pH = AddConstant(fsParameterIdentifier.Ph);
+            m_pHcake = AddVariable(fsParameterIdentifier.PHcake);
+            m_cakeWashOutContent = AddVariable(fsParameterIdentifier.CakeWashOutContent);
+
+            #endregion
+
+            Equations = null;
+        }
+
+        public fsCalculationOptions.fsFromCalculationOption FromCalculationOption;
+
+        public fsCalculationOptions.fsWashOutContentOption WashOutContentOption;
+
+        public void RebuildEquationsList()
+        {
+            Equations = new List<fsCalculatorEquation>();
+
+            var zero = new fsCalculatorConstant(new fsParameterIdentifier("zero"))
+            {
+                IsInput = true,
+                Value = fsValue.Zero
+            };
+            AddEquation(new fsMoistureContentEquation(m_cakeMoistureContent, m_dryMass, m_wetMass, zero));
+
+            if (FromCalculationOption == fsCalculationOptions.fsFromCalculationOption.WashOutContent)
+            {
+                if (WashOutContentOption == fsCalculationOptions.fsWashOutContentOption.AsMassFraction)
+                {
+                    AddEquation(new fsCakeWashOutContentEquation(m_cakeWashOutContent, m_dryMass, m_wetMass, m_liquidMassForResuspension, m_solidsMassFraction));
+                }
+                else
+                {
+                    m_internalC.IsInput = false;
+                    AddEquation(new fsProductEquation(m_solidsConcentration, m_internalC, m_liquidDensity));
+                    AddEquation(new fsCakeWashOutContentEquation(m_cakeWashOutContent, m_dryMass, m_wetMass, m_liquidMassForResuspension, m_internalC));
+                }
+            }
+            else
+            {
+                m_internalC.IsInput = false;
+                AddEquation(new fsPhCakeEquation(m_pHcake, m_pH, m_wetMass, m_dryMass, m_liquidMassForResuspension));
+                AddEquation(new fsConcentrationFromPhEquation(m_internalC, m_pH, m_liquidDensity));
+                AddEquation(new fsCakeWashOutContentEquation(m_cakeWashOutContent, m_dryMass, m_wetMass, m_liquidMassForResuspension, m_internalC));
+            }
+        }
+    }
+}
