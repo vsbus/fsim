bb571a5ca034431e3e08f2a3c85d4d5a2d92a760
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-17 21:10:40 +0000

    recreate andrew/dev

---
+++ andrew/dev/fsim/CalculatorModules/LaboratoryFiltrationTime.cs
@@ -0,0 +1,136 @@
+﻿using System.Drawing;
+using CalculatorModules.Base_Controls;
+using Parameters;
+using StepCalculators;
+using Value;
+using System.Windows.Forms;
+
+namespace CalculatorModules
+{
+    public sealed partial class fsLaboratoryFiltrationTime : fsOptionsSingleTableAndCommentsCalculatorControl
+    {
+        protected override void InitializeCalculators()
+        {
+            Calculators.Add(new fsDensityConcentrationCalculator());
+            Calculators.Add(new fsPorosityCalculator());
+            Calculators.Add(new fsLaboratoryFiltrationCalculator());
+        }
+
+        protected override void InitializeGroups()
+        {
+            fsParametersGroup filtrateGroup = AddGroup(
+               fsParameterIdentifier.MotherLiquidDensity);
+            fsParametersGroup solidsGroup = AddGroup(
+                fsParameterIdentifier.SolidsDensity,
+                fsParameterIdentifier.SuspensionDensity);
+            fsParametersGroup concentrationGroup = AddGroup(
+                fsParameterIdentifier.SuspensionSolidsMassFraction,
+                fsParameterIdentifier.SuspensionSolidsVolumeFraction,
+                fsParameterIdentifier.SuspensionSolidsConcentration);
+            fsParametersGroup neGroup = AddGroup(
+                fsParameterIdentifier.Ne);
+            fsParametersGroup ncGroup = AddGroup(
+                fsParameterIdentifier.CakeCompressibility);
+            fsParametersGroup epsKappaGroup = AddGroup(
+                fsParameterIdentifier.CakePorosity0,
+                fsParameterIdentifier.DryCakeDensity0,
+                fsParameterIdentifier.Kappa0,
+                fsParameterIdentifier.CakeMoistureContentRf0,
+                fsParameterIdentifier.CakePorosity,
+                fsParameterIdentifier.DryCakeDensity,
+                fsParameterIdentifier.Kappa,
+                fsParameterIdentifier.CakeMoistureContentRf);
+            fsParametersGroup viscosityGroup = AddGroup(
+                fsParameterIdentifier.MotherLiquidViscosity);
+            fsParametersGroup pc0Rc0Alpha0Group = AddGroup(
+                fsParameterIdentifier.CakePermeability0,
+                fsParameterIdentifier.CakeResistance0,
+                fsParameterIdentifier.CakeResistanceAlpha0,
+                fsParameterIdentifier.CakePermeability,
+                fsParameterIdentifier.CakeResistance,
+                fsParameterIdentifier.CakeResistanceAlpha,
+                fsParameterIdentifier.PracticalCakePermeability);
+            fsParametersGroup hceGroup = AddGroup(
+                fsParameterIdentifier.FilterMediumResistanceHce,
+                fsParameterIdentifier.FilterMediumResistanceRm);
+            fsParametersGroup areaGroup = AddGroup(
+                fsParameterIdentifier.FilterArea);
+            fsParametersGroup pressureGroup = AddGroup(
+                fsParameterIdentifier.PressureDifference);
+            fsParametersGroup cakeFormationGroup = AddGroup(
+                fsParameterIdentifier.FiltrationTime,
+                fsParameterIdentifier.CakeHeight,
+                fsParameterIdentifier.SuspensionMass,
+                fsParameterIdentifier.FiltrateMass,
+                fsParameterIdentifier.CakeMass,
+                fsParameterIdentifier.SolidsMass,
+                fsParameterIdentifier.qmf,
+                fsParameterIdentifier.SuspensionVolume,
+                fsParameterIdentifier.FiltrateVolume,
+                fsParameterIdentifier.CakeVolume,
+                fsParameterIdentifier.SolidsVolume,
+                fsParameterIdentifier.qf);
+
+            var groups = new[]
+                             {
+                                 viscosityGroup,
+                                 filtrateGroup,
+                                 solidsGroup,
+                                 concentrationGroup,
+                                 epsKappaGroup,
+                                 neGroup,
+                                 ncGroup,
+                                 pc0Rc0Alpha0Group,
+                                 hceGroup,
+                                 areaGroup,
+                                 pressureGroup,
+                                 cakeFormationGroup,
+                             };
+
+            var colors = new[]
+                             {
+                                 Color.FromArgb(255, 255, 230),
+                                 Color.FromArgb(255, 230, 255),
+                                 Color.FromArgb(230, 255, 255)
+                             };
+
+            for (int i = 0; i < groups.Length; ++i)
+            {
+                groups[i].SetIsInputFlag(true);
+                AddGroupToUI(dataGrid, groups[i], colors[i % colors.Length]);
+            }
+
+            ParameterToCell[fsParameterIdentifier.Ne].OwningRow.Visible = false;
+            ParameterToCell[fsParameterIdentifier.CakePorosity0].OwningRow.Visible = false;
+            ParameterToCell[fsParameterIdentifier.DryCakeDensity0].OwningRow.Visible = false;
+            ParameterToCell[fsParameterIdentifier.Kappa0].OwningRow.Visible = false;
+            ParameterToCell[fsParameterIdentifier.CakeMoistureContentRf0].OwningRow.Visible = false;
+        }
+
+        protected override Control[] GetUIControlsToConnectWithDataUpdating()
+        {
+            return new Control[] { dataGrid };
+        }
+
+        public fsLaboratoryFiltrationTime()
+        {
+            InitializeComponent();
+        }
+
+        protected override void InitializeParametersValues()
+        {
+            Values[fsParameterIdentifier.Ne].Value = new fsValue(0);
+        }
+
+
+        protected override void UpdateEquationsFromCalculationOptions()
+        {
+            // this control has only one equation
+        }
+
+        protected override void UpdateGroupsInputInfoFromCalculationOptions()
+        {
+            // this control hasn't calculation options
+        }
+    }
+}
---
+++ andrew/dev/fsim/CalculatorModules/User Controls/Help Dialogs/fsTablesAndChartsParametersSelectionDialog.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Parameters;
+
+namespace CalculatorModules.User_Controls.Help_Dialogs
+{
+    public partial class fsTablesAndChartsParametersSelectionDialog : Form
+    {
+        public fsTablesAndChartsParametersSelectionDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButtonClick(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void OkButtonClick(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        internal List<fsParameterIdentifier> GetCheckedYAxisParameters()
+        {
+            return y1SelectionControl.GetCheckedYAxisParameters();
+        }
+
+        internal List<fsParameterIdentifier> GetCheckedY2AxisParameters()
+        {
+            return y2SelectionControl.GetCheckedYAxisParameters();
+        }
+
+        internal void AssignYAxisParameters(List<fsYAxisParameterWithChecking> list)
+        {
+            y1SelectionControl.AssignYAxisParameters(list);
+        }
+
+        internal void AssignY2AxisParameters(List<fsYAxisParameterWithChecking> list)
+        {
+            y2SelectionControl.AssignYAxisParameters(list);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetY2Visible(splitContainer1.Panel2Collapsed);
+            
+        }
+
+        private void fsTablesAndChartsParametersSelectionDialog_Load(object sender, EventArgs e)
+        {
+            SetY2Visible(y2SelectionControl.GetCheckedYAxisParameters().Count != 0);
+        }
+
+        private void SetY2Visible(bool isVisible)
+        {
+            if (isVisible == !splitContainer1.Panel2Collapsed)
+                return;
+
+            if (isVisible)
+            {
+                splitContainer1.Panel2Collapsed = false;
+                Width = Width * 2;
+            }
+            else
+            {
+                splitContainer1.Panel2Collapsed = true;
+                Width = Width / 2;
+            }
+        }
+    }
+}
