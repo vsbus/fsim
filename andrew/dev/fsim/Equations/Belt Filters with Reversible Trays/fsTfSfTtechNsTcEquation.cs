c91ba37b7ab3106dd6f6157e4febe425d65fcf73
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-06-18 13:03:23 +0000

    recreate /dev

---
+++ andrew/dev/fsim/Equations/Belt Filters with Reversible Trays/fsTfSfTtechNsTcEquation.cs
@@ -0,0 +1,52 @@
+﻿using Parameters;
+
+namespace Equations.Belt_Filters_with_Reversible_Trays
+{
+    public class fsTfSfTtechNsTcEquation : fsCalculatorEquation
+    {
+        #region Parameters
+
+        private readonly IEquationParameter m_sf;
+        private readonly IEquationParameter m_tf;
+        private readonly IEquationParameter m_tc;
+        private readonly IEquationParameter m_ns;
+        private readonly IEquationParameter m_ttech;
+        
+        #endregion
+
+        public fsTfSfTtechNsTcEquation(
+            IEquationParameter sf,
+            IEquationParameter tf,
+            IEquationParameter tc,
+            IEquationParameter ns,
+            IEquationParameter ttech)
+            : base(sf, tf, tc, ns, ttech)
+        {
+            m_sf = sf;
+            m_tf = tf;
+            m_tc = tc;
+            m_ns = ns;
+            m_ttech = ttech;
+        }
+
+        protected override void InitFormulas()
+        {
+            AddFormula(m_sf, SfFormula);
+            AddFormula(m_tf, TfFormula);
+        }
+
+        #region Formulas
+
+        private void SfFormula()
+        {
+            m_sf.Value = m_tf.Value / (m_tc.Value - m_ns.Value * m_ttech.Value);
+        }
+
+        private void TfFormula()
+        {
+            m_tf.Value = m_sf.Value * (m_tc.Value - m_ns.Value * m_ttech.Value);
+        }
+
+        #endregion
+    }
+}
---
+++ andrew/dev/fsim/fsUIControls/fsParametersCheckBoxesTree.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Parameters;
+
+namespace fsUIControls
+{
+    public partial class fsParametersCheckBoxesTree : UserControl
+    {
+        private Dictionary<TreeNode, fsParameterIdentifier> m_leafsToParameters = new Dictionary<TreeNode, fsParameterIdentifier>();
+        private Dictionary<fsParameterIdentifier, bool> m_involvedParametersWithCheckStatus = new Dictionary<fsParameterIdentifier, bool>();
+
+        public fsParametersCheckBoxesTree()
+        {
+            InitializeComponent();
+
+        }
+
+        #region Tree Initialization
+
+        public void InitializeTree(Dictionary<fsParameterIdentifier, bool> involvedParametersWithCheckStatus)
+        {
+            m_involvedParametersWithCheckStatus = involvedParametersWithCheckStatus;
+
+            treeView1.Nodes.Clear();
+
+            AddGroupToTree("Densities", treeView1.Nodes,
+                new[]
+                {
+                    fsParameterIdentifier.MotherLiquidDensity,
+                    fsParameterIdentifier.LiquidDensity,
+                    fsParameterIdentifier.SolidsDensity,
+                    fsParameterIdentifier.SuspensionDensity
+                });
+
+            AddGroupToTree("Concentrations", treeView1.Nodes,
+                new[]
+                {
+                    fsParameterIdentifier.SuspensionSolidsMassFraction,
+                    fsParameterIdentifier.SuspensionSolidsVolumeFraction,
+                    fsParameterIdentifier.SuspensionSolidsConcentration
+                });
+
+            AddGroupToTree("eps0, kappa0 (Dp = 1)", treeView1.Nodes,
+                new[]
+                {
+                    fsParameterIdentifier.Ne,
+                    fsParameterIdentifier.CakePorosity0,
+                    fsParameterIdentifier.Kappa0,
+                    fsParameterIdentifier.DryCakeDensity0
+                });
+
+            AddGroupToTree("eps, kappa", treeView1.Nodes,
+                new[]
+                {
+                    fsParameterIdentifier.CakePorosity,
+                    fsParameterIdentifier.Kappa,
+                    fsParameterIdentifier.DryCakeDensity
+                });
+
+            AddGroupToTree("Pc0, rc0, alpha0", treeView1.Nodes,
+                new[]
+                {
+                    fsParameterIdentifier.CakePermeability0,
+                    fsParameterIdentifier.CakeResistance0,
+                    fsParameterIdentifier.CakeResistanceAlpha0
+                });
+            AddGroupToTree("n, tc, tr", treeView1.Nodes,
+                new[]
+                {
+                    fsParameterIdentifier.NumberOfCyclones,                
+                    fsParameterIdentifier.RotationalSpeed,
+                    fsParameterIdentifier.ResidualTime
+                });
+
+            treeView1.ExpandAll();
+        }
+
+        private void AddGroupToTree(
+            string nodeName,
+            TreeNodeCollection treeNodeCollection,
+            IEnumerable<fsParameterIdentifier> parameters)
+        {
+            var groupNode = new TreeNode(nodeName);
+            int checkedLeafsCount = 0;
+            foreach (var parameterIdentifier in parameters)
+            {
+                if (m_involvedParametersWithCheckStatus.ContainsKey(parameterIdentifier))
+                {
+                    TreeNode leaf = groupNode.Nodes.Add(parameterIdentifier.ToString());
+                    if (m_involvedParametersWithCheckStatus[parameterIdentifier])
+                    {
+                        leaf.Checked = true;
+                        ++checkedLeafsCount;
+                    }
+                    m_leafsToParameters.Add(leaf, parameterIdentifier);
+                }
+            }
+            if (groupNode.Nodes.Count > 0)
+            {
+                groupNode.Checked = groupNode.Nodes.Count == checkedLeafsCount;
+                treeNodeCollection.Add(groupNode);
+            }
+        }
+
+        #endregion
+
+        public Dictionary<fsParameterIdentifier, bool> GetParametersToShowAndHide()
+        {
+            var parametersToShowAndHide = new Dictionary<fsParameterIdentifier, bool>();
+            foreach (TreeNode node in treeView1.Nodes)
+            {
+                AddCheckedParametersToList(node, parametersToShowAndHide);
+            }
+            return parametersToShowAndHide;
+        }
+
+        private void AddCheckedParametersToList(TreeNode node, Dictionary<fsParameterIdentifier, bool> checkedParameters)
+        {
+            if (node.Nodes.Count == 0)
+            {
+                checkedParameters.Add(m_leafsToParameters[node], node.Checked);
+            }
+            else
+            {
+                foreach (TreeNode subNode in node.Nodes)
+                {
+                    AddCheckedParametersToList(subNode, checkedParameters);
+                }
+            }
+        }
+
+
+    }
+}
