ee7111941b6b83292d07a8d1d498e6c019c58759
victorbarinov <victorbarinov@1ac1e342-90af-751f-0d3c-ce704170c97d>
2012-07-09 13:37:37 +0000

    disable broken modules

--- andrew/dev_calc_redesign/fsim/StepCalculators/Small Calculators/fsRfFromWetDryCakeCalculator.cs
+++ andrew/dev_calc_redesign/fsim/StepCalculators/Small Calculators/fsRfFromWetDryCakeCalculator.cs
@@ -6,60 +6,60 @@ using Value;
 
 namespace StepCalculators
 {
-    public class fsRfFromWetDryCakeCalculator : fsCalculator
-    {
-        readonly fsCalculatorConstant m_wetMass;
-        readonly fsCalculatorConstant m_dryMass;
-        readonly fsCalculatorConstant m_solidsMassFraction;
-        readonly fsCalculatorConstant m_solidsConcentration;
-        readonly fsCalculatorConstant m_liquidDensity;
-        readonly fsCalculatorVariable m_cakeMoistureContent;
-        readonly fsCalculatorVariable m_internalC;
-
-        public fsRfFromWetDryCakeCalculator()
-        {
-            #region Parameters Initialization
-
-            m_wetMass = AddConstant(fsParameterIdentifier.WetCakeMass);
-            m_dryMass = AddConstant(fsParameterIdentifier.DryCakeMass);
-            m_solidsMassFraction = AddConstant(fsParameterIdentifier.SolutesMassFractionInLiquid);
-            m_solidsConcentration = AddConstant(fsParameterIdentifier.SolutesConcentrationInCakeLiquid);
-            m_liquidDensity = AddConstant(fsParameterIdentifier.LiquidDensity);
-            m_cakeMoistureContent = AddVariable(fsParameterIdentifier.CakeMoistureContentRf);
-            m_internalC = AddVariable(new fsParameterIdentifier("internalC"));
-
-            #endregion
-
-            Equations = null;
-        }
-
-        public fsCalculationOptions.fsSaltContentOption SaltContentOption;
-        public fsCalculationOptions.fsConcentrationOption ConcentrationOption;
-
-        public void RebuildEquationsList()
-        {
-            Equations = new List<fsCalculatorEquation>();
-
-            if (SaltContentOption == fsCalculationOptions.fsSaltContentOption.Neglected)
-            {
-                m_internalC.Value = fsValue.Zero;
-                m_internalC.IsInput = true;
-                m_internalC.IsProcessed = true;
-                AddEquation(new fsMoistureContentEquation(m_cakeMoistureContent, m_dryMass, m_wetMass, m_internalC));
-            }
-            else
-            {
-                if (ConcentrationOption == fsCalculationOptions.fsConcentrationOption.SolidsMassFraction)
-                {
-                    AddEquation(new fsMoistureContentEquation(m_cakeMoistureContent, m_dryMass, m_wetMass, m_solidsMassFraction));
-                }
-                else
-                {
-                    m_internalC.IsInput = false;
-                    AddEquation(new fsProductEquation(m_solidsConcentration, m_internalC, m_liquidDensity));
-                    AddEquation(new fsMoistureContentEquation(m_cakeMoistureContent, m_dryMass, m_wetMass, m_internalC));
-                }
-            }
-        }
-    }
+//     public class fsRfFromWetDryCakeCalculator : fsCalculatorEquationsList
+//     {
+//         readonly fsCalculatorConstant m_wetMass;
+//         readonly fsCalculatorConstant m_dryMass;
+//         readonly fsCalculatorConstant m_solidsMassFraction;
+//         readonly fsCalculatorConstant m_solidsConcentration;
+//         readonly fsCalculatorConstant m_liquidDensity;
+//         readonly fsCalculatorVariable m_cakeMoistureContent;
+//         readonly fsCalculatorVariable m_internalC;
+// 
+//         public fsRfFromWetDryCakeCalculator()
+//         {
+//             #region Parameters Initialization
+// 
+//             m_wetMass = calculator.AddVariable(fsParameterIdentifier.WetCakeMass);
+//             m_dryMass = calculator.AddVariable(fsParameterIdentifier.DryCakeMass);
+//             m_solidsMassFraction = calculator.AddVariable(fsParameterIdentifier.SolutesMassFractionInLiquid);
+//             m_solidsConcentration = calculator.AddVariable(fsParameterIdentifier.SolutesConcentrationInCakeLiquid);
+//             m_liquidDensity = calculator.AddVariable(fsParameterIdentifier.LiquidDensity);
+//             m_cakeMoistureContent = calculator.AddVariable(fsParameterIdentifier.CakeMoistureContentRf);
+//             m_internalC = calculator.AddVariable(new fsParameterIdentifier("internalC"));
+// 
+//             #endregion
+// 
+//             Equations = null;
+//         }
+// 
+//         public fsCalculationOptions.fsSaltContentOption SaltContentOption;
+//         public fsCalculationOptions.fsConcentrationOption ConcentrationOption;
+// 
+//         public void RebuildEquationsList()
+//         {
+//             Equations = new List<fsCalculatorEquation>();
+// 
+//             if (SaltContentOption == fsCalculationOptions.fsSaltContentOption.Neglected)
+//             {
+//                 m_internalC.Value = fsValue.Zero;
+//                 m_internalC.IsInput = true;
+//                 m_internalC.IsProcessed = true;
+//                 calculator.AddEquation(new fsMoistureContentEquation(m_cakeMoistureContent, m_dryMass, m_wetMass, m_internalC));
+//             }
+//             else
+//             {
+//                 if (ConcentrationOption == fsCalculationOptions.fsConcentrationOption.SolidsMassFraction)
+//                 {
+//                     calculator.AddEquation(new fsMoistureContentEquation(m_cakeMoistureContent, m_dryMass, m_wetMass, m_solidsMassFraction));
+//                 }
+//                 else
+//                 {
+//                     m_internalC.IsInput = false;
+//                     calculator.AddEquation(new fsProductEquation(m_solidsConcentration, m_internalC, m_liquidDensity));
+//                     calculator.AddEquation(new fsMoistureContentEquation(m_cakeMoistureContent, m_dryMass, m_wetMass, m_internalC));
+//                 }
+//             }
+//         }
+//     }
 }
